080a77920189504fce4f3a7fcea691540135c9bc
Lee Byron <lee@leebyron.com>
2016-09-12 22:01:34 -0700

    Redirect template

---
+++ site/_core/Redirect.js
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2015, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+var React = require('react');
+
+export default ({ to }) =>
+  <html>
+    <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="refresh" content={'1;url=' + to} />
+      <script dangerouslySetInnerHTML={{__html: `
+        window.location.href = "${to}"
+      `}} />
+      <title>GraphQL Page Redirection</title>
+    </head>
+    <body>
+      If you are not redirected automatically, <a href={to}>follow this link</a>.
+    </body>
+  </html>
