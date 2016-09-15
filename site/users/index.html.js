86ec5a1d800da58501a7f15a9a3d2e2f0092c84d
Lee Byron <lee@leebyron.com>
2016-09-15 14:09:18 -0500

    Squashed commit of the following:
    commit 2e38d82e7bd3014a8bb5cab79f662ae9c89f784c
    Merge: cb45ee5 53528f0
    Author: Lee Byron <lee@leebyron.com>
    Date:   Thu Sep 15 14:07:48 2016 -0500
    
        Merge branch 'source' of https://github.com/danahmadi/graphql.github.io into danahmadi-source
    
    commit 53528f0927c2886ae56ab0e318b0f488a0ecf33a
    Author: danahmadi <dan@meteor.com>
    Date:   Thu Sep 15 11:57:24 2016 -0700
    
        Swap Meteor Link back to Story of how we use GraphQL
    
    commit cbdea8a9178de24d5d6494636f32e2705c5ea6bf
    Author: danahmadi <dan@meteor.com>
    Date:   Thu Sep 15 11:56:13 2016 -0700
    
        Removed Apollo Logo, Trimmed whitespace around Meteor Logo
    
    commit c0abb7471047ed2433ca351dd0b78aeeaae2761e
    Author: danahmadi <dan@meteor.com>
    Date:   Thu Sep 15 11:55:02 2016 -0700
    
        Remove Apollo, Add Meteor Link
    
    commit 28d5a1f7f56ddd4ebc58e9d2884a7bdd9a1081d6
    Author: danahmadi <dan@meteor.com>
    Date:   Thu Sep 15 11:54:33 2016 -0700
    
        Added trailing comma, merged branches
    
    commit 6f3f63f69b607a220f186ecdf35e41b47d434a81
    Author: danahmadi <dan@meteor.com>
    Date:   Thu Sep 15 11:46:54 2016 -0700
    
        Added bracket back in!
    
    commit 940c49784b42ee87b02b1abba61caea7b2505e22
    Author: danahmadi <dan@meteor.com>
    Date:   Thu Sep 15 11:45:15 2016 -0700
    
        Extra space removed
    
    commit ccda0e997705627d25b7c29e559292dbf9ba5f58
    Merge: 49a3d4d e69baf8
    Author: danahmadi <dan@meteor.com>
    Date:   Thu Sep 15 11:44:49 2016 -0700
    
        Merge remote-tracking branch 'graphql/source' into source
    
        # Conflicts:
        #	site/users/index.html.js
    
    commit 49a3d4d87e935d27b4858cf81368615d9a0af629
    Author: danahmadi <dan@meteor.com>
    Date:   Thu Sep 15 11:35:06 2016 -0700
    
        Added Meteor and Apollo to the GraphQL user list
    
    commit 79d99f3ff39aedeed4c8eaf4d3bbe97015ee9f25
    Author: danahmadi <dan@meteor.com>
    Date:   Thu Sep 15 11:34:49 2016 -0700
    
        Added logos

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -54,6 +54,11 @@ var logos = [
     isRound: true,
     link: 'https://www.shopify.com/'
   },
+  {
+    name: 'Meteor',
+    img: 'meteor.png',
+    link: 'https://medium.com/apollo-stack/snappier-uis-with-apollo-client-graphql-bffab0b52b79#.fr92aru2y'
+  },
 ];
 
 module.exports = ({ page }) =>
