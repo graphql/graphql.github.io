d13866408581ccd6f8ba46e2328096bcce0fc779
Kaosisochukwu Uzokwe <kuzokwe@gmail.com>
2016-09-17 09:52:59 -0400

    Add alphasights to users

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -136,6 +136,11 @@ var logos = [
     img: 'allocine.png',
     link: 'http://www.allocine.fr'
   },
+  {
+    name: 'Alphasights',
+    img: 'alphasights.png',
+    link: 'https://engineering.alphasights.com'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
