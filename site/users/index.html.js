ddd7feaf9e53809a58aee9ce0e881b9f1094b57c
Bruno Lemos <brunohplemos@gmail.com>
2016-09-18 14:57:36 -0300

    Added Easy Carros - Brazilian Startup (#117)

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -136,6 +136,11 @@ var logos = [
     img: 'allocine.png',
     link: 'http://www.allocine.fr'
   },
+  {
+    name: 'Easy Carros',
+    img: 'easycarros.png',
+    link: 'http://www.easycarros.com'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
