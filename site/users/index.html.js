cf08ade3331f4a7125d236a48009a3acf5b5f98c
Nik Graf <nik@nikgraf.com>
2016-09-18 14:58:48 +0200

    add serverless

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -110,6 +110,11 @@ var logos = [
     img: 'inerva.png',
     link: 'http://www.inerva.com'
   },
+  {
+    name: 'Serverless',
+    img: 'serverless.png',
+    link: 'https://serverless.com/'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
