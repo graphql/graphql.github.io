22851c126786642054ffb8378097dd85fdf6886c
Martijn Walraven <martijn@martijnwalraven.com>
2016-10-22 14:30:05 -0700

    Add Apollo iOS to GraphQL Clients section

--- site/code/index.html.js
+++ site/code/index.html.js
@@ -313,9 +313,15 @@ Code that executes a hello world GraphQL query with \`graphql-clj\`:
 
 ## GraphQL Clients
 
+### JavaScript
+
   - [Relay](https://facebook.github.io/relay/) ([github](https://github.com/facebook/relay)) ([npm](https://www.npmjs.com/package/react-relay)): Facebook's framework for building React applications that talk to a GraphQL backend.
   - [Apollo Client](http://dev.apollodata.com/react/) ([github](https://github.com/apollostack/apollo-client)): A powerful JavaScript GraphQL client, designed to work well with React, React Native, Angular 2, or just plain JavaScript.
   - [Lokka](https://github.com/kadirahq/lokka): A simple JavaScript GraphQL client that works in all JavaScript environments - the browser, Node.js, and React Native.
+
+### Swift / iOS
+
+- [Apollo iOS](http://dev.apollodata.com/ios/) ([github](https://github.com/apollostack/apollo-ios)): A GraphQL client for iOS that returns results as query-specific Swift types, and integrates with Xcode to show your Swift source and GraphQL side by side, with inline validation errors.
 
 ## Tools
 
