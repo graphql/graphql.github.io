5efda6f0e5173206a9d3299e4ceb5375336fbc22
Lee Byron <lee@leebyron.com>
2016-10-27 12:48:34 -0700

    pure component for Site wrapper

--- site/_core/Site.js
+++ site/_core/Site.js
@@ -11,102 +11,99 @@ var HeaderLinks = require('./HeaderLinks');
 var Search = require('./Search');
 var SiteData = require('./SiteData');
 
-var Site = React.createClass({
-  render: function() {
-    var page = this.props.page;
-    var suffix = this.props.category || 'GraphQL';
-    var pageTitle = this.props.title ?
-      `${this.props.title} | ${suffix}` :
-      `GraphQL | ${SiteData.description}`;
-    return (
-      <html>
-        <head>
-          <meta charSet="utf-8" />
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-          <title>{pageTitle}</title>
-          <meta name="viewport" content="width=640" />
-          <meta property="og:title" content="GraphQL: A query language for APIs." />
-          <meta property="og:description" content="GraphQL provides a complete description of the data in your API, gives clients the power to ask for exactly what they need and nothing more, makes it easier to evolve APIs over time, and enables powerful developer tools." />
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content="http://graphql.org/" />
-          <meta property="og:image" content="/img/og_image.png" />
-          <meta property="og:image:type" content="image/png" />
-          <meta name="twitter:card" content="summary" />
-          <meta name="twitter:site" content="@graphql" />
-          <meta name="twitter:title" content="GraphQL: A query language for APIs." />
-          <meta name="twitter:description" content="GraphQL gives clients the power to ask for exactly what they need and nothing more, making it easier to evolve APIs over time." />
-          <meta name="twitter:image" content="/img/twitter_image.png" />
-          <link rel="shortcut icon" href="/img/favicon.png" />
-          <link rel="home" type="application/rss+xml" href="/blog/rss.xml" title="GraphQL Team Blog" />
-          <link rel="stylesheet" href="/style.css" />
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Rubik:300|Roboto:300" />
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Mono:400,400i,600" />
-          <link rel="stylesheet" href="https://cdn.jsdelivr.net/docsearch.js/1/docsearch.min.css" />
-        </head>
-        <body className={this.props.className}>
+export default ({ page, category, title, section, className, noSearch, children }) =>
+  <html>
+    <head>
+      <meta charSet="utf-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+      <title>
+        {title ?
+          `${title} | ${category || 'GraphQL'}` :
+          `GraphQL | ${SiteData.description}`}
+      </title>
+      <meta name="viewport" content="width=640" />
+      <meta property="og:title" content="GraphQL: A query language for APIs." />
+      <meta property="og:description" content="GraphQL provides a complete description of the data in your API, gives clients the power to ask for exactly what they need and nothing more, makes it easier to evolve APIs over time, and enables powerful developer tools." />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="http://graphql.org/" />
+      <meta property="og:image" content="/img/og_image.png" />
+      <meta property="og:image:type" content="image/png" />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="@graphql" />
+      <meta name="twitter:title" content="GraphQL: A query language for APIs." />
+      <meta name="twitter:description" content="GraphQL gives clients the power to ask for exactly what they need and nothing more, making it easier to evolve APIs over time." />
+      <meta name="twitter:image" content="/img/twitter_image.png" />
+      <link rel="shortcut icon" href="/img/favicon.png" />
+      <link rel="home" type="application/rss+xml" href="/blog/rss.xml" title="GraphQL Team Blog" />
+      <link rel="stylesheet" href="/style.css" />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Rubik:300|Roboto:300" />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Mono:400,400i,600" />
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/docsearch.js/1/docsearch.min.css" />
+    </head>
+    <body className={className}>
 
-          <header>
-            <section>
-              <a className="nav-home" href="/">
-                <img className="nav-logo" src="/img/logo.svg" width="30" height="30" />
-                GraphQL
-              </a>
-              <HeaderLinks section={this.props.section} />
-              {this.props.noSearch || <Search />}
-            </section>
-          </header>
+      <header>
+        <section>
+          <a className="nav-home" href="/">
+            <img className="nav-logo" src="/img/logo.svg" width="30" height="30" />
+            GraphQL
+          </a>
+          <HeaderLinks section={section} />
+          {noSearch || <Search />}
+        </section>
+      </header>
 
-          {this.props.children}
+      {children}
 
-          <footer>
-            <section className="sitemap">
-              <a href="/" className="nav-home" />
-              <div>
-                <h5><a href="/learn/">Learn</a></h5>
-                <a href="/learn/">Introduction</a>
-                <a href="/learn/queries/">Query Language</a>
-                <a href="/learn/schema/">Type System</a>
-                <a href="/learn/execution/">Execution</a>
-                <a href="/learn/best-practices/">Best Practices</a>
-              </div>
-              <div>
-                <h5><a href="/code">Code</a></h5>
-                <a href="/code/#graphql-server-libraries">Servers</a>
-                <a href="/code/#graphql-clients">Clients</a>
-                <a href="/code/#tools">Tools</a>
-              </div>
-              <div>
-                <h5><a href="/community">Community</a></h5>
-                <a href="/community/upcoming-events/">Upcoming Events</a>
-                <a href="http://stackoverflow.com/questions/tagged/graphql" target="_blank" rel="noopener noreferrer">Stack Overflow</a>
-                <a href="https://www.facebook.com/groups/graphql.community/" target="_blank" rel="noopener noreferrer">Facebook Group</a>
-                <a href="https://twitter.com/GraphQL" target="_blank" rel="noopener noreferrer">Twitter</a>
-              </div>
-              <div>
-                <h5>More</h5>
-                <a href="/blog">GraphQL Team Blog</a>
-                <a href="http://facebook.github.io/graphql/" target="_blank" rel="noopener noreferrer">Read the Spec</a>
-                <a href="https://github.com/graphql" target="_blank" rel="noopener noreferrer">GitHub</a>
-                {page && <a href={'https://github.com/graphql/graphql.github.io/edit/source/site/' + page.relPath} target="_blank" rel="noopener noreferrer">Edit this page &#x270E;</a>}
-              </div>
-            </section>
-            <a href="https://code.facebook.com/projects/" target="_blank" rel="noopener noreferrer" className="fbOpenSource">
-              <img src="/img/oss_logo.png" alt="Facebook Open Source" width="170" height="45" />
-            </a>
-            <section className="copyright">
-              Copyright &copy; 2016 Facebook Inc. The contents of this page are licensed BSD-3-Clause.
-            </section>
-          </footer>
+      <footer>
+        <section className="sitemap">
+          <a href="/" className="nav-home" />
+          <div>
+            <h5><a href="/learn/">Learn</a></h5>
+            <a href="/learn/">Introduction</a>
+            <a href="/learn/queries/">Query Language</a>
+            <a href="/learn/schema/">Type System</a>
+            <a href="/learn/execution/">Execution</a>
+            <a href="/learn/best-practices/">Best Practices</a>
+          </div>
+          <div>
+            <h5><a href="/code">Code</a></h5>
+            <a href="/code/#graphql-server-libraries">Servers</a>
+            <a href="/code/#graphql-clients">Clients</a>
+            <a href="/code/#tools">Tools</a>
+          </div>
+          <div>
+            <h5><a href="/community">Community</a></h5>
+            <a href="/community/upcoming-events/">Upcoming Events</a>
+            <a href="http://stackoverflow.com/questions/tagged/graphql" target="_blank" rel="noopener noreferrer">Stack Overflow</a>
+            <a href="https://www.facebook.com/groups/graphql.community/" target="_blank" rel="noopener noreferrer">Facebook Group</a>
+            <a href="https://twitter.com/GraphQL" target="_blank" rel="noopener noreferrer">Twitter</a>
+          </div>
+          <div>
+            <h5>More</h5>
+            <a href="/blog">GraphQL Team Blog</a>
+            <a href="http://facebook.github.io/graphql/" target="_blank" rel="noopener noreferrer">Read the Spec</a>
+            <a href="https://github.com/graphql" target="_blank" rel="noopener noreferrer">GitHub</a>
+            {page && <a href={'https://github.com/graphql/graphql.github.io/edit/source/site/' + page.relPath} target="_blank" rel="noopener noreferrer">Edit this page &#x270E;</a>}
+          </div>
+        </section>
+        <a href="https://code.facebook.com/projects/" target="_blank" rel="noopener noreferrer" className="fbOpenSource">
+          <img src="/img/oss_logo.png" alt="Facebook Open Source" width="170" height="45" />
+        </a>
+        <section className="copyright">
+          Copyright &copy; 2016 Facebook Inc. The contents of this page are licensed BSD-3-Clause.
+        </section>
+      </footer>
 
-          <script type="text/javascript" src="https://cdn.jsdelivr.net/docsearch.js/1/docsearch.min.js"></script>
-          <script dangerouslySetInnerHTML={{__html: `
-            docsearch({
-              apiKey: 'd103541f3e6041148aade2e746ed4d61',
-              indexName: 'graphql',
-              inputSelector: '#algolia-search-input'
-            });
-          `}} />
-          <script dangerouslySetInnerHTML={{__html: `
+      <script type="text/javascript" src="https://cdn.jsdelivr.net/docsearch.js/1/docsearch.min.js"></script>
+      <script dangerouslySetInnerHTML={{__html: `
+        docsearch({
+          apiKey: 'd103541f3e6041148aade2e746ed4d61',
+          indexName: 'graphql',
+          inputSelector: '#algolia-search-input'
+        });
+      `}} />
+      <script dangerouslySetInnerHTML={{__html: `
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -114,11 +111,6 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-44373548-16', 'auto');
 ga('send', 'pageview');
-          `}} />
-        </body>
-      </html>
-    );
-  }
-});
-
-module.exports = Site;
+      `}} />
+    </body>
+  </html>
