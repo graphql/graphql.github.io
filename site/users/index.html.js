bde667b829869939b530ef32b49b5255ca714ca8
danielgriese <d.griese@gmail.com>
2016-10-27 08:16:54 +0200

    Update index.html.js

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -266,6 +266,11 @@ var logos = [
     img: 'ningensoft.png',
     link: 'https://www.ningensoft.com/'
   },
+  {
+    name: 'zlyde',
+    img: 'zlyde.png',
+    link: 'http://www.zyde.com/'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
