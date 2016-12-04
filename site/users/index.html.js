c814e43848db18c860dd5da64ec4871a6bb3e9ee
Steve Gacka <steve.gacka@startups.co>
2016-12-04 02:37:39 -0500

    Add Startups.co to users page.

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -349,6 +349,11 @@ var logos = [
     img: 'waitlessq.png',
     link: 'http://waitlessq.com/'
   },
+  {
+    name: 'Startups.co',
+    img: 'startupsco.png',
+    link: 'https://www.startups.co/'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
