c41031fc9c0f9b41283d554db07ee5e6ff3970d8
Tuukka Hastrup <Tuukka.Hastrup@iki.fi>
2017-06-17 14:29:48 +0300

    add Digitransit to users

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -465,9 +465,9 @@ var logos = [
     link: 'https://www.circlehd.com/',
   },
   {
-    name: 'MyHeritage',
-    img: 'myheritage.png',
-    link: 'https://www.myheritage.com',
+    name: 'Digitransit',
+    img: 'digitransit.png',
+    link: 'https://digitransit.fi/',
   },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
