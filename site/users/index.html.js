7479c51962b83ad8ac44d4c30f75a883d6392a44
Tzook Shaked <Tzook@users.noreply.github.com>
2017-06-18 14:45:54 +0300

    Add MyHeritage logo to the list

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -480,6 +480,11 @@ var logos = [
     img: 'circlehd.png',
     link: 'https://www.circlehd.com/',
   },
+  {
+    name: 'MyHeritage',
+    img: 'myheritage.png',
+    link: 'https://www.myheritage.com',
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
