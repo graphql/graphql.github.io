017ff9bb3944c818e616f5966e5e28855db18363
Bruno Soares <bruno@bsoares.com.br>
2017-07-18 14:03:43 -0300

    Add GetNinjas logo to GraphQL users (#353)

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -500,6 +500,11 @@ var logos = [
     isRound: true,
     link: 'https://etmdb.com'
   },
+  {
+    name: 'GetNinjas',
+    img: 'getninjas.png',
+    link: 'https://labs.getninjas.com.br/tagged/graphql'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
