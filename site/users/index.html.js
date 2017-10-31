42036b35adbf8eda7823ae5fcc4f41d2b358020b
Márcio Vicente <marciovicente.filho@gmail.com>
2017-10-31 00:39:12 -0300

    Adding Jusbrasil entry in index.html.js

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -596,6 +596,11 @@ var logos = [
     img: 'bynder.png',
     link: 'https://www.bynder.com'
   },
+  {
+    name: 'Jusbrasil',
+    img: 'jusbrasil.png',
+    link: 'https://www.jusbrasil.com.br'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
