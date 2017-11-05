54e306632d93d79e5d3b3d5a1b397e06a7ce3851
Tim Buckley <timothypbuckley@gmail.com>
2017-11-04 22:33:01 -0400

    Add 2U to users list

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -596,6 +596,11 @@ var logos = [
     img: 'bynder.png',
     link: 'https://www.bynder.com'
   },
+  {
+    name: '2U',
+    img: '2u.png',
+    link: 'https://2u.com/'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
