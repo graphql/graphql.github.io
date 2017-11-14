ab348ddf73234a22aef473a2020a8ac9f64c702c
PatrickJS <github@gdi2290.com>
2017-11-14 02:14:43 -0800

    feat: tipe.io

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -596,6 +596,11 @@ var logos = [
     img: 'bynder.png',
     link: 'https://www.bynder.com'
   },
+  {
+    name: 'Tipe',
+    img: 'tipe.png',
+    link: 'https://tipe.io'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
