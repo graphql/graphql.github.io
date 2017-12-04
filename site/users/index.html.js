31f214f4d73122076f12786eea8f86ba594b3b00
Duy Tai Nguyen <duytai.cse@gmail.com>
2017-12-04 10:26:16 +0700

    add mollivn to site

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -596,6 +596,11 @@ var logos = [
     img: 'bynder.png',
     link: 'https://www.bynder.com'
   },
+  {
+    name: 'Molli Vietnam',
+    img: 'mollivn.png',
+    link: 'http://www.molli.vn'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
