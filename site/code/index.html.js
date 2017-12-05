95a9de8767f7d191de77ce6c312af7a323f0b4a3
Pyojo <pyojo@protonmail.com>
2017-12-05 09:39:30 -0300

    Update index.html.js

--- site/code/index.html.js
+++ site/code/index.html.js
@@ -425,6 +425,7 @@ Executor.execute(schema, query) map println
 ### Python
 
   - [GQL](https://github.com/graphql-python/gql): A GraphQL client in Python.
+  - [python-graphql-client](https://github.com/graphcool/python-graphql-client): Simple GraphQL client for Python 2.7+.
 
 ## Tools
 
