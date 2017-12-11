aa78535cc879e3717afb6d9407262ef35f93bc80
Roman Bartusiak <riomus@gmail.com>
2017-12-11 11:53:08 +0100

    Adding IamBot logo to users

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -596,6 +596,11 @@ var logos = [
     img: 'bynder.png',
     link: 'https://www.bynder.com'
   },
+  {
+    name: 'IamBot',
+    img: 'iambot.png',
+    link: 'https://iambot.ai'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
