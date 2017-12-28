2db9cb23cf0b14c04dfd7790d8020f7c038d0afe
normannsky <normann.sky@gmail.com>
2017-12-28 14:52:19 -0700

    Add Xledger to the users page

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -596,6 +596,11 @@ var logos = [
     img: 'bynder.png',
     link: 'https://www.bynder.com'
   },
+  {
+    name: 'Xledger',
+    img: 'xledger.png',
+    link: 'https://xledger.com'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
