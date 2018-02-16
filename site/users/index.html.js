1d6a1cd2bf9d00dbd1056b26893e464acca7573f
Christoffer Niska <ChristofferNiska@gmail.com>
2018-02-16 14:29:21 +0200

    Add Digia to users

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -596,6 +596,11 @@ var logos = [
     img: 'bynder.png',
     link: 'https://www.bynder.com'
   },
+  {
+    name: 'Digia',
+    img: 'digia.png',
+    link: 'https://digiaonline.github.io'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
