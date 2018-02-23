0c01c898db97326fcabae929febd2b500caf185a
Scott <banruosheng@gmail.com>
2018-02-23 19:11:13 +0800

    Added Songxiaocai to users

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -596,6 +596,11 @@ var logos = [
     img: 'bynder.png',
     link: 'https://www.bynder.com'
   },
+  {
+    name: '宋小菜',
+    img: 'songxiaocai.png',
+    link: 'https://www.songxiaocai.com/'
+  }
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
