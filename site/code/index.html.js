2636c020c0e48a18b6d0cf26e776d61ba669c9e7
Mohit Ahuja <ahujamoh@gmail.com>
2018-03-20 10:46:49 +0100

    fixing code

--- site/code/index.html.js
+++ site/code/index.html.js
@@ -322,8 +322,8 @@ import graphene
 class Query(graphene.ObjectType):
   hello = graphene.String(name=graphene.String(default_value="World"))
 
-    def resolve_hello(self, info, name):
-        return 'Hello ' + name
+  def resolve_hello(self, info, name):
+    return 'Hello ' + name
 
 schema = graphene.Schema(query=Query)
 result = schema.execute('{ hello }')
