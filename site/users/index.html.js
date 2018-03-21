1fb09ca808a2cff2287b2a691a97eaa188e1508a
Praveen D <praveen@hasura.io>
2018-03-21 15:50:48 +0530

    added hasura to users list

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -596,6 +596,11 @@ var logos = [
     img: 'bynder.png',
     link: 'https://www.bynder.com'
   },
+  {
+    name: 'Hasura',
+    img: 'hasura.png',
+    link: 'https://hasura.io'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
