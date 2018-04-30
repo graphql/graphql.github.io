5761a9c607a00243215f9cb9108ebb2b630e018e
Mohamed Alansary <mohamed_alansary@rocketmail.com>
2018-04-29 22:02:00 -0400

    Update index.html.js

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -601,6 +601,11 @@ var logos = [
     img: 'universe.png',
     link: 'https://www.universe.com'
   },
+  {
+    name: 'Appsinnovate',
+    img: 'appsinnovate.png',
+    link: 'http://appsinnovate.com/'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
