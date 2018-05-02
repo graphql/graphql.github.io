18e109f55fd149b971b4e78bdb827ae732831c62
Jacky Tang <jackytck@gmail.com>
2018-05-02 15:46:36 +0800

    Add Altizure to users page

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -601,6 +601,11 @@ var logos = [
     img: 'universe.png',
     link: 'https://www.universe.com'
   },
+  {
+    name: 'Altizure',
+    img: 'altizure.png',
+    link: 'https://www.altizure.com'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
