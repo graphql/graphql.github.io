d780db758eac08148fd3ad30abdfe10f1c880fd4
ktmq <kmckew@jooraccess.com>
2018-05-09 13:38:32 -0400

    Add JOOR to users page
    Adding the our company logo to the users page.

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -601,6 +601,11 @@ var logos = [
     img: 'universe.png',
     link: 'https://www.universe.com'
   },
+  {
+    name: 'JOOR',
+    img: 'joor.png',
+    link: 'https://jooraccess.com/'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
