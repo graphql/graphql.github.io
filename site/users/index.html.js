f47992382885482aa0b70273152eda3fc949a6b6
Paul Matthew Jaworski <32399626+pauljaws@users.noreply.github.com>
2018-06-28 22:54:54 -0700

    Add StackShare (#511)
    * add StackShare
    
    * Add StackShare logo

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -606,6 +606,11 @@ var logos = [
     link: 'https://www.universe.com'
   },
   {
+    name: 'StackShare',
+    img: 'stackshare.png',
+    link: 'https://stackshare.io'
+  },
+  {
     name: 'Metric.ai',
     img: 'metric-ai.png',
     link: 'https://www.metric.ai'
