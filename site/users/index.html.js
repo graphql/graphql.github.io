c76c5b7b1df9e02e8163aa950ccbef753389aff3
Pierre Burgy <pierre.burgy@gmail.com>
2018-07-01 13:37:45 +0200

    Add Strapi

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -655,7 +655,12 @@ var logos = [
     name: 'UC Trends',
     img: 'uctrends.png',
     link: 'https://trends.ucweb.com'
-  }
+  },
+  {
+    name: 'Strapi',
+    img: 'strapi.png',
+    link: 'https://strapi.io'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
