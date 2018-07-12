199df7c23d2929c3740310b3b3b0c105392aea18
Sebastian Müller <mail@sbstjn.com>
2018-07-12 11:22:43 +0200

    Add Jimdo to list of GraphQL users

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -655,7 +655,12 @@ var logos = [
     name: 'UC Trends',
     img: 'uctrends.png',
     link: 'https://trends.ucweb.com'
-  }
+  },
+  {
+    name: 'Jimdo GmbH',
+    img: 'jimdo.png',
+    link: 'https://jimdo.com'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
