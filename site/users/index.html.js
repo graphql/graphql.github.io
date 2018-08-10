d61ce2c485b81deb1474707d685572e6efc4716e
Ty Soehngen <ty@stardog.com>
2018-08-10 10:20:42 -0400

    Adds Stardog to /users page

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -655,7 +655,12 @@ var logos = [
     name: 'UC Trends',
     img: 'uctrends.png',
     link: 'https://trends.ucweb.com'
-  }
+  },
+  {
+    name: 'Stardog Union',
+    img: 'stardog.png',
+    link: 'https://stardog.com'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
