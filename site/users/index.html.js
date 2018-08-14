418d8a86a76b04367db3ba3552285969c8d40a69
Shane Laymance <slaymanc@starbucks.com>
2018-08-14 16:12:33 -0700

    Add Starbucks logo

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -655,7 +655,13 @@ var logos = [
     name: 'UC Trends',
     img: 'uctrends.png',
     link: 'https://trends.ucweb.com'
-  }
+  },
+  {
+    name: 'Starbucks',
+    img: 'starbucks.png',
+    link: 'https://www.starbucks.com/',
+    isRound: true
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
