947ea5c0c2ab1f869e0aeeee9d5084de741347e3
Cory Monty <cmonty@users.noreply.github.com>
2018-10-01 10:46:57 -0500

    Adds Braintree to users

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -655,6 +655,11 @@ var logos = [
     name: 'UC Trends',
     img: 'uctrends.png',
     link: 'https://trends.ucweb.com'
+  },
+  {
+    name: 'Braintree',
+    img: 'braintree-paypal-logo-black.png',
+    link: 'https://www.braintreepayments.com/'
   }
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
