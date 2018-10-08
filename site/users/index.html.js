af55143e9deaad14c560b0746a1e89b89bdfaed4
Rémi <r.janot@gmail.com>
2018-10-08 18:26:39 +0200

    [WHEELIZ] Add wheeliz

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -655,6 +655,11 @@ var logos = [
     name: 'UC Trends',
     img: 'uctrends.png',
     link: 'https://trends.ucweb.com'
+  },
+  {
+    name: 'Wheeliz',
+    img: 'wheeliz.png',
+    link: 'https://www.wheeliz.com'
   }
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
