365f8a5da60f3392869345b5e531aaf774d3b313
Auriga <AG-Systems@users.noreply.github.com>
2018-10-13 13:00:42 -0700

    Added ChronoCI

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -655,7 +655,12 @@ var logos = [
     name: 'UC Trends',
     img: 'uctrends.png',
     link: 'https://trends.ucweb.com'
-  }
+  },
+  {
+    name: 'ChronoCI',
+    img: 'chronoci.png',
+    link: 'https://www.chronoci.com/'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
