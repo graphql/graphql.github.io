50deabfe0d754a35aecc4933f5c73eedc0515360
Michael Staib <michael.staib@hotmail.com>
2018-10-23 00:53:35 +0200

    Renamed hot chcocolate description from service to server (#594)

--- site/code/index.html.js
+++ site/code/index.html.js
@@ -42,7 +42,7 @@ In addition to the GraphQL [reference implementations in JavaScript](#javascript
 
   - [graphql-dotnet](https://github.com/graphql-dotnet/graphql-dotnet): GraphQL for .NET
   - [graphql-net](https://github.com/ckimes89/graphql-net): Convert GraphQL to IQueryable
-  - [Hot Chocolate](https://github.com/ChilliCream/hotchocolate): GraphQL Service for .net core and .net classic
+  - [Hot Chocolate](https://github.com/ChilliCream/hotchocolate): GraphQL Server for .net core and .net classic
 
 ### Clojure
 
