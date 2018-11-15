c42d28b268916a26fbaf560f0f8b7bbb1d9144ae
Claire Tran <c.tran83@gmail.com>
2018-11-15 12:40:32 +1100

    Adding expert360 to users (#605)

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -657,6 +657,11 @@ var logos = [
     link: 'https://trends.ucweb.com'
   },
   {
+    name: 'Expert360',
+    img: 'expert360.png',
+    link: 'https://expert360.com'
+  },
+  {
     name: 'Cloverleaf',
     img: 'cloverleaf.png',
     link: 'https://cloverleaf.me/'
