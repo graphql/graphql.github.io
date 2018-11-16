100f698ed0d414d90de577445d688fb5e538f6b8
Benedikt Franke <benedikt@franke.tech>
2018-11-16 07:56:17 -0200

    Update site/code/index.html.js
    Co-Authored-By: robsontenorio <robsontenorio@me.com>

--- site/code/index.html.js
+++ site/code/index.html.js
@@ -337,7 +337,7 @@ Apollo Server also supports all Node.js HTTP server frameworks: Express, Connect
 
   - [graphql-php](https://github.com/webonyx/graphql-php): A PHP port of GraphQL reference implementation
   - [graphql-relay-php](https://github.com/ivome/graphql-relay-php): A library to help construct a graphql-php server supporting react-relay.
-  - [lighthouse](https://github.com/nuwave/lighthouse): GraphQL Server for Laravel
+  - [Lighthouse](https://github.com/nuwave/lighthouse): GraphQL Server for Laravel
 
 #### [Siler](https://siler.leocavalcante.com/graphql/) ([github](https://github.com/leocavalcante/siler))
 
