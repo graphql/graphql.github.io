56ff942c3d9ae0ad06f970465a0d77f91fdc29d1
Hans Zwolsman <hans@denieuwezaak.nl>
2018-11-23 08:53:23 +0100

    Added ecManager logo to users.

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -666,6 +666,11 @@ var logos = [
     img: 'cloverleaf.png',
     link: 'https://cloverleaf.me/'
   },
+  {
+    name: 'ecManager',
+    img: 'ecmanager.png',
+    link: 'https://www.ecmanager.nl/en/'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
