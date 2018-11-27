08b1eb8fe62256740431ccfc64c602e5407973f6
Cane Lagerwaard <40865297+cane-lagerwaard@users.noreply.github.com>
2018-11-27 20:30:43 +0100

    add deity

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -666,6 +666,11 @@ var logos = [
     img: 'cloverleaf.png',
     link: 'https://cloverleaf.me/'
   },
+  {
+    name: 'DEITY',
+    img: 'deity.png',
+    link: 'https://github.com/deity-io/falcon'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
