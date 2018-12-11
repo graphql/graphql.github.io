666b165885cdbed5287c43cdb03b8a86de059fe5
azihsoyn <azihsoyn@gmail.com>
2018-12-11 10:48:42 +0900

    add gunosy

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -666,6 +666,11 @@ var logos = [
     img: 'cloverleaf.png',
     link: 'https://cloverleaf.me/'
   },
+  {
+    name: 'Gunosy',
+    img: 'gunosy.png',
+    link: 'https://gunosy.com/'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
