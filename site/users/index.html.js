76c639a78acb025c090818e6c14751f2cc866ed3
Oscar Pérez <oscar@xmltravelgate.com>
2019-01-18 09:41:23 +0100

    Add TravelgateX logo

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -666,6 +666,11 @@ var logos = [
     img: 'cloverleaf.png',
     link: 'https://cloverleaf.me/'
   },
+  {
+    name: 'TravelgateX',
+    img: 'travelgatex.png',
+    link: 'https://www.travelgatex.com/'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
