13ea21f15f46b0e32432e7cc0e574df399c439b3
karthikraobr <karthik.0703@gmail.com>
2019-01-18 12:42:32 +0100

    Added travel audience to users page

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -666,6 +666,11 @@ var logos = [
     img: 'cloverleaf.png',
     link: 'https://cloverleaf.me/'
   },
+  {
+    name: 'Travel Audience',
+    img: 'travelaudience.png',
+    link: 'https://www.travelaudience.com/'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
