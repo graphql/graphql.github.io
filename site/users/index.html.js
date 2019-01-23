4b142d83f364a07f83a845c1019c6d3dea6fdaa6
Jacek <jacek.budzynski.92@gmail.com>
2019-01-23 10:10:03 +0000

    Add Administrate to users page

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -666,6 +666,11 @@ var logos = [
     img: 'cloverleaf.png',
     link: 'https://cloverleaf.me/'
   },
+  {
+    name: 'Administrate',
+    img: 'administrate.png',
+    link: 'https://getadministrate.com/'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
