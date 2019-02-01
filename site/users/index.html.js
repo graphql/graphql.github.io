4bc26e5c2d535cfa7df0468a598f797eadc95b03
Chase Willden <chase.willden@gmail.com>
2019-02-01 11:47:12 -0700

    Update index.html.js

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -666,6 +666,11 @@ var logos = [
     img: 'cloverleaf.png',
     link: 'https://cloverleaf.me/'
   },
+  {
+    name: 'Stukent',
+    img: 'stukent.png',
+    link: 'https://stukent.com'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
