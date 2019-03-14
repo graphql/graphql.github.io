a799cf6f6b87806992a13236e91bda3c75e6e589
BigMaster <liuning@baixing.com>
2019-03-14 14:41:41 +0800

    Add compamy baixing inc

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -666,6 +666,11 @@ var logos = [
     img: 'cloverleaf.png',
     link: 'https://cloverleaf.me/'
   },
+  {
+    name: 'Baixing',
+    img: 'baixing.png',
+    link: 'http://www.baixing.com/'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
