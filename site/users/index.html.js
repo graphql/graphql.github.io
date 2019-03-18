9e8cbbb98d065eb001eae31c8b1c9f8011337878
Jose Leon <jrleon84@gmail.com>
2019-03-18 19:35:56 +0100

    Add MoonMail logo to users page

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -666,6 +666,11 @@ var logos = [
     img: 'cloverleaf.png',
     link: 'https://cloverleaf.me/'
   },
+  {
+    name: 'MoonMail',
+    img: 'moonmail.png',
+    link: 'https://moonmail.io/'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
