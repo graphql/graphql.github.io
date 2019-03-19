91c063e72e5fbca2a8e679627f85889cef9a233f
Tomáš Szabo <tomas.szabo@deftomat.com>
2019-03-19 13:24:13 +0100

    add filejet logo

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -666,6 +666,11 @@ var logos = [
     img: 'cloverleaf.png',
     link: 'https://cloverleaf.me/'
   },
+  {
+    name: 'FileJet',
+    img: 'filejet.png',
+    link: 'https://filejet.io'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
