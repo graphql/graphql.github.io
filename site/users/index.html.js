de052ef78fd18a90a355b061b7e454b2e14ed1e4
William Wanyama <william.wanyamaopondo@kloeckner.com>
2019-03-20 14:54:14 +0100

    Add Kloeckner-i logo

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -666,6 +666,11 @@ var logos = [
     img: 'cloverleaf.png',
     link: 'https://cloverleaf.me/'
   },
+  {
+    name: 'Kloeckner-i',
+    img: 'kloeckner.png',
+    link: 'https://www.kloeckner-i.com/en'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
