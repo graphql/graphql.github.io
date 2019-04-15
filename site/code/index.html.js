2a09653e2a844902e371fe1fa80c0558458127c4
John Biundo <johnbiundo@users.noreply.github.com>
2019-04-15 16:57:50 -0700

    Doc proposed change: Add NestJs
    NestJs implements an Apollo module, and seems to make sense to add to this list.

--- site/code/index.html.js
+++ site/code/index.html.js
@@ -329,7 +329,7 @@ app.listen({ port: 4000 }, () =>
 );
 \`\`\`
 
-Apollo Server also supports all Node.js HTTP server frameworks: Express, Connect, HAPI and Koa.
+Apollo Server also supports all Node.js HTTP server frameworks: Express, Connect, HAPI, Koa and NestJs.
 
 ### Kotlin
 
