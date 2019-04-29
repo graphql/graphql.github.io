60f415aab9e3fb1ca8ac0265b579c90ebe8b66b4
Felix Wu <felixwudev@gmail.com>
2019-04-29 16:05:24 +1000

    Add Company Skychute

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -666,6 +666,11 @@ var logos = [
     img: 'cloverleaf.png',
     link: 'https://cloverleaf.me/'
   },
+  {
+    name: 'Skychute',
+    img: 'skychute.png',
+    link: 'https://www.skychute.com.au/'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
