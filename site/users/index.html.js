fd9187f159b2853e13f23da55f876881f93a64e3
lgoncalves <exnlesi@gmail.com>
2019-05-08 17:42:44 +0100

    add Taikai logo
    https://taikai.network

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -666,6 +666,11 @@ var logos = [
     img: 'cloverleaf.png',
     link: 'https://cloverleaf.me/'
   },
+  {
+    name: 'Taikai',
+    img: 'taikai.png',
+    link: 'https://taikai.network/'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
