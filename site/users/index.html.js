0d74762c18dc51c0c5c5034412abc1d9a6d0bb19
Cody Reichert <cody@reichertbrothers.com>
2019-06-04 15:12:14 -0500

    Add Assertible to users page

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -666,6 +666,11 @@ var logos = [
     img: 'cloverleaf.png',
     link: 'https://cloverleaf.me/'
   },
+  {
+    name: 'Assertible',
+    img: 'assertible.png',
+    link: 'https://assertible.com/'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
