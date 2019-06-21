5f5a31e5145c385c258f5f1f9e9772229816f544
fanny <fanny.vieira@ccc.ufcg.edu.br>
2019-06-21 09:27:14 -0300

    Add vtex

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -666,6 +666,11 @@ var logos = [
     img: 'cloverleaf.png',
     link: 'https://cloverleaf.me/'
   },
+  {
+    name: 'VTEX',
+    img: 'vtex.png',
+    link: 'https://www.vtex.com/'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
