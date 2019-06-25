0b829f7e05dec2ede3c4b60e476876ce37ce8d64
Maciek Stasieluk <macrusher@gmail.com>
2019-06-25 15:11:36 +0200

    Add Vazco to users

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -666,6 +666,11 @@ var logos = [
     img: 'cloverleaf.png',
     link: 'https://cloverleaf.me/'
   },
+  {
+    name: 'Vazco',
+    img: 'vazco.png',
+    link: 'https://www.vazco.eu/graphql'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
