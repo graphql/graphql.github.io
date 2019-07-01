fb4fcac147b42f306b0055c39c3cc498ccba88c8
Bruno Lima <bruno@fidelize.com.br>
2019-07-01 17:10:00 -0300

    Funcional Helth Tech logo added

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -666,6 +666,12 @@ var logos = [
     img: 'cloverleaf.png',
     link: 'https://cloverleaf.me/'
   },
+  {
+    name: 'Funcional Health Tech',
+    img: 'funcionallogo.png',
+    link: 'https://www.funcionalcorp.com.br/'
+  },
+
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
