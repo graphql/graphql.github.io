4dfb6434b3ae809961948e2918fbfd12844ee76a
Christoph Engelbert (noctarius) <me@noctarius.com>
2019-07-16 12:54:19 +0200

    added Instana to GraphQL users
    Signed-off-by: Christoph Engelbert (noctarius) <me@noctarius.com>

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -666,6 +666,11 @@ var logos = [
     img: 'cloverleaf.png',
     link: 'https://cloverleaf.me/'
   },
+  {
+  	name: 'Instana',
+  	img: 'instana.png',
+  	link: 'https://www.instana.com'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
