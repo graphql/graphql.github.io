d9b4c65f593f1e9e899042e5c26f7b10391e93b1
Mateus Inocencio Maia <mateus_maaia@hotmail.com>
2019-07-19 11:08:21 -0300

    Adding dafiti into clients

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -666,6 +666,11 @@ var logos = [
     img: 'cloverleaf.png',
     link: 'https://cloverleaf.me/'
   },
+  {
+    name: 'Dafiti Group',
+    img: 'dafiti_group.png',
+    link: 'https://dafiti.com.br'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
