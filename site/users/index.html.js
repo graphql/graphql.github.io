a47e8f2c813c04972fd50204eca84d7a06167549
Simon Franzen <simon@zauberware.com>
2019-07-25 12:06:59 +0200

    added zauberware.com to users of graphql

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -666,6 +666,11 @@ var logos = [
     img: 'cloverleaf.png',
     link: 'https://cloverleaf.me/'
   },
+  {
+    name: 'Zauberware',
+    img: 'zauberware.png',
+    link: 'https://www.zauberware.com/'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
