e862fb05dea4437466d61364bd81987a23fcb10e
benburton <benjamin.joseph.burton@gmail.com>
2019-07-25 13:11:24 -0400

    Add The Orchard

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -666,6 +666,11 @@ var logos = [
     img: 'cloverleaf.png',
     link: 'https://cloverleaf.me/'
   },
+  {
+    name: 'The Orchard',
+    img: 'the-orchard.png',
+    link: 'https://www.theorchard.com/'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
