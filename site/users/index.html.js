779f8ed06bad96c9651f60121df47f5c806f62a5
Szymon Łągiewka <slagiewka@users.noreply.github.com>
2019-07-30 11:00:57 +0200

    Add Brainly to the GQL users list.

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -666,6 +666,11 @@ var logos = [
     img: 'cloverleaf.png',
     link: 'https://cloverleaf.me/'
   },
+  {
+    name: 'Brainly',
+    img: 'brainly.png',
+    link: 'https://www.brainly.com/'
+  },
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
