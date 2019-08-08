1f4811db087d63bbc9931b1058996f4bfa7949cf
laredoj <laredoj@us.ibm.com>
2019-08-08 14:52:23 -0400

    Added IBM as a GraphQL User.
    Not only we use it extensively internally, we are also embedding GraphQL interfaces in some of our products, we have open source packages like OpenAPI-to-GraphQL and IBM is a founding member of the GraphQL Foundation.

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -666,6 +666,11 @@ var logos = [
     img: 'cloverleaf.png',
     link: 'https://cloverleaf.me/'
   },
+  {
+    name: 'IBM',
+    img: 'ibm.png',
+    link: 'https://ibm.com'
+  },  
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
