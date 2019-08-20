4f69d0f364c3203511a574a54c49e15424b77fe9
Chris Aniszczyk <caniszczyk@gmail.com>
2019-08-19 21:41:47 -0500

    Update to standard LF footer boilerplate

--- site/_core/Site.js
+++ site/_core/Site.js
@@ -88,7 +88,7 @@ export default ({ page, category, title, section, className, noSearch, children 
           </div>
         </section>
         <section className="copyright">
-          Copyright © 2019 GraphQL a Series of LF Projects, LLC. For web site terms of use, trademark policy and other project policies please see https://lfprojects.org.  
+           Copyright © 2019 The GraphQL Foundation. All rights reserved. The Linux Foundation has registered trademarks and uses trademarks. For a list of trademarks of The Linux Foundation, please see our <a href="https://www.linuxfoundation.org/trademark-usage>Trademark Usage</a> page. Linux is a registered trademark of Linus Torvalds. <a href="http://www.linuxfoundation.org/privacy">Privacy Policy</a> and <a href="http://www.linuxfoundation.org/terms">Terms of Use</a>.
         </section>
       </footer>
 
