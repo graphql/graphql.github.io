be957b8626110aa608aa7ada190985a89c9cbb7d
Yury <yurka7321@gmail.com>
2019-09-18 11:51:05 +0600

    add  \ 'es to `

--- site/code/index.html.js
+++ site/code/index.html.js
@@ -295,7 +295,7 @@ const app = express();
 server.applyMiddleware({ app });
 
 app.listen({ port: 4000 }, () =>
-  console.log(`🚀 Now browse to http://localhost:4000${server.graphqlPath}`)
+  console.log(\`🚀 Now browse to http://localhost:4000${server.graphqlPath}\`)
 );
 \`\`\`
 
