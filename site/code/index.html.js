0d195f34dfe2a26e275983b116556ae74ad5b89c
Timur Murtukov <murtukov@gmail.com>
2019-11-26 12:14:29 +0100

    Add a Symfony bundle into the PHP section

--- site/code/index.html.js
+++ site/code/index.html.js
@@ -340,7 +340,8 @@ Apollo Server also supports all Node.js HTTP server frameworks: Express, Connect
 
   - [graphql-php](https://github.com/webonyx/graphql-php): A PHP port of GraphQL reference implementation
   - [graphql-relay-php](https://github.com/ivome/graphql-relay-php): A library to help construct a graphql-php server supporting react-relay.
-  - [Lighthouse](https://github.com/nuwave/lighthouse): GraphQL Server for Laravel
+  - [Lighthouse](https://github.com/nuwave/lighthouse): A GraphQL server for Laravel
+  - [GraphQLBundle](https://github.com/overblog/GraphQLBundle): A GraphQL server for Symfony
 
 #### [Siler](https://siler.leocavalcante.com/graphql/) ([github](https://github.com/leocavalcante/siler))
 
