c90d3f344f2211a9186c394d6d2e28ca88758fe9
Jason Bahl <jasonbahl@mac.com>
2020-02-20 11:46:19 -0700

    Add WPGraphQL to the list of PHP libraries

--- site/code/index.html.js
+++ site/code/index.html.js
@@ -351,6 +351,7 @@ Apollo Server also supports all Node.js HTTP server frameworks: Express, Connect
   - [graphql-relay-php](https://github.com/ivome/graphql-relay-php): A library to help construct a graphql-php server supporting react-relay.
   - [Lighthouse](https://github.com/nuwave/lighthouse): A GraphQL server for Laravel
   - [GraphQLBundle](https://github.com/overblog/GraphQLBundle): A GraphQL server for Symfony
+  - [WPGraphQL](https://github.com/wp-graphql/wp-graphql): A free, open-source WordPress plugin that provides an extendable GraphQL schema and API for any WordPress site 
 
 #### [GraphQLite](https://graphqlite.thecodingmachine.io) ([github](https://github.com/thecodingmachine/graphqlite))
 
