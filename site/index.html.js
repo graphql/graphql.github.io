19405c7ad248e7ad19a9509995326d3479898a20
can meng <canmeng@122.bjo.hetao101.com>
2020-04-16 18:55:40 +0800

    test travis ci

--- site/index.html.js
+++ site/index.html.js
@@ -25,7 +25,7 @@ module.exports = ({ page, section }) =>
           </div>
 
           <div className="marketing-col">
-            <h3>Describe your data</h3>
+            <h3>Describe your data mengcan</h3>
             <Prism language="graphql">
               {`type Project {
   name: String
