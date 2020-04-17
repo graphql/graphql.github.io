1f630f161181f6ecf67a013b93c0bdddccd266f7
can meng <canmeng@122.local>
2020-04-17 11:32:55 +0800

    ./

--- site/_core/Site.js
+++ site/_core/Site.js
@@ -27,16 +27,16 @@ export default ({ page, category, title, section, className, noSearch, children 
       <meta property="og:description" content="GraphQL provides a complete description of the data in your API, gives clients the power to ask for exactly what they need and nothing more, makes it easier to evolve APIs over time, and enables powerful developer tools." />
       <meta property="og:type" content="website" />
       <meta property="og:url" content="http://graphql.org/" />
-      <meta property="og:image" content="/img/og_image.png" />
+      <meta property="og:image" content="../img/og_image.png" />
       <meta property="og:image:type" content="image/png" />
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:site" content="@graphql" />
       <meta name="twitter:title" content="GraphQL: A query language for APIs." />
       <meta name="twitter:description" content="GraphQL gives clients the power to ask for exactly what they need and nothing more, making it easier to evolve APIs over time." />
-      <meta name="twitter:image" content="/img/twitter_image.png" />
-      <link rel="shortcut icon" href="/img/favicon.png" />
-      <link rel="home" type="application/rss+xml" href="/blog/rss.xml" title="GraphQL Team Blog" />
-      <link rel="stylesheet" href="./style.css" />
+      <meta name="twitter:image" content="../img/twitter_image.png" />
+      <link rel="shortcut icon" href="../img/favicon.png" />
+      <link rel="home" type="application/rss+xml" href="../blog/rss.xml" title="GraphQL Team Blog" />
+      <link rel="stylesheet" href="../style.css" />
       <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Rubik:300|Roboto:300" />
       <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Mono:400,400i,600" />
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/docsearch.js/1/docsearch.min.css" />
@@ -46,7 +46,7 @@ export default ({ page, category, title, section, className, noSearch, children 
       <header>
         <section>
           <a className="nav-home" href="./">
-            <img className="nav-logo" src="/img/logo.svg" alt="GraphQL Logo" width="30" height="30" />
+            <img className="nav-logo" src="../img/logo.svg" alt="GraphQL Logo" width="30" height="30" />
             GraphQL
           </a>
           <HeaderLinks section={section} />
