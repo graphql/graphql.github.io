e62df8c34d75285178c8a9aeff6a0b867a39ea12
saihaj <44710980+saihaj@users.noreply.github.com>
2020-07-06 19:32:58 -0500

    feat: 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
 import Layout from '../components/Layout'
-import SEO from '../components/Seo'
 
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  <Layout title="404: Not found">
+    <div style={{ marginLeft: '7rem', marginBottom: '16rem' }}>
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </div>
   </Layout>
 )
 
