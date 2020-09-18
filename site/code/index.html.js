578ad4d94f037db259c7416b4f1344d5cfee7f14
Deswerve Pushdere <s4+deswerve@digitale-oberlausitz.eu>
2020-09-18 13:26:12 +0000

    Add kepawni/serge to the list of PHP libraries

--- site/code/index.html.js
+++ site/code/index.html.js
@@ -355,6 +355,7 @@ Apollo Server also supports all Node.js HTTP server frameworks: Express, Connect
   - [Lighthouse](https://github.com/nuwave/lighthouse): A GraphQL server for Laravel.
   - [GraphQLBundle](https://github.com/overblog/GraphQLBundle): A GraphQL server for Symfony.
   - [WPGraphQL](https://github.com/wp-graphql/wp-graphql): A free, open-source WordPress plugin that provides an extendable GraphQL schema and API for any WordPress site 
+  - [serge](https://github.com/kepawni/serge): Use GraphQL to define your Domain Model for CQRS/ES and let serge generate code to handle GraphQL requests.
 
 #### [API Platform](https://api-platform.com) ([github](https://github.com/api-platform/api-platform))
 
