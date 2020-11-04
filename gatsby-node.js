1b6ad56edabd9e6979e53a1b6fe55b8cebebdae5
Arda TANRIKULU <ardatanrikulu@gmail.com>
2020-11-04 10:29:45 +0300

    Fix Learn Sidebar

--- gatsby-node.js
+++ gatsby-node.js
@@ -148,7 +148,6 @@ exports.createPages = async ({ graphql, actions }) => {
         const { category: definedCategory, next: definedNextPageUrl } = frontmatter
         let category = definedCategory || folder
         if (!currentCategory || category !== currentCategory.name) {
-          currentCategory && categories.push(currentCategory)
           currentCategory = {
             name: category,
             links: [],
