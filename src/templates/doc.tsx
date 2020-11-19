133792096a2591b406428079d77747122b3378dd
carolstran <carolynstransky@gmail.com>
2020-11-19 13:59:00 +0100

    Initial design - not pretty

--- src/templates/doc.tsx
+++ src/templates/doc.tsx
@@ -21,7 +21,7 @@ const layoutMap: any = {
 const Blog = ({ data, pageContext }: Props) => {
   const {
     doc: {
-      frontmatter: { title, date, permalink, byline, guestBio, layout, questions },
+      frontmatter: { title, date, permalink, byline, guestBio, layout, gettingStartedQuestions, generalQuestions, bestPracticesQuestions, specificationQuestions, frontendQuestions },
       rawMarkdownBody,
     },
     nextDoc,
@@ -35,7 +35,11 @@ const Blog = ({ data, pageContext }: Props) => {
         permalink={permalink}
         byline={byline}
         guestBio={guestBio}
-        questions={questions}
+        gettingStartedQuestions={gettingStartedQuestions}
+        generalQuestions={generalQuestions}
+        bestPracticesQuestions={bestPracticesQuestions}
+        specificationQuestions={specificationQuestions}
+        frontendQuestions={frontendQuestions}
         rawMarkdownBody={rawMarkdownBody}
         nextDoc={nextDoc}
         sideBarData={pageContext.sideBarData}
@@ -56,7 +60,11 @@ export const query = graphql`
         guestBio
         sublinks
         layout
-        questions
+        gettingStartedQuestions
+        generalQuestions
+        bestPracticesQuestions
+        specificationQuestions
+        frontendQuestions
       }
       id
       rawMarkdownBody
