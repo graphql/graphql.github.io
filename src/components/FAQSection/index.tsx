abe18e5240a19041af562dc2b253fd7676d1520c
carolstran <carolynstransky@gmail.com>
2020-12-03 19:38:09 +0100

    Fix initial bugs with accordion

--- src/components/FAQSection/index.tsx
+++ src/components/FAQSection/index.tsx
@@ -1,32 +1,20 @@
 import React from "react"
-import { Link } from "gatsby"
 import Marked from "../Marked"
-import { toSlug } from '../../utils/slug'
 
 interface Props {
   title: string
   permalink: string
-  questions: string
   rawMarkdownBody: string
   pageContext: any
 }
 
 const FAQSection = ({
   title,
-  questions,
   rawMarkdownBody,
   pageContext
 }: Props) => (
   <section className="inner-content">
     <h2>{title}</h2>
-    {questions
-        .split(',')
-        .map(question => (
-            <Link className="faq-questions" key={question} to={`#${toSlug( question )}`}>
-                {question}
-            </Link>
-        ))
-    }
     <Marked pageContext={pageContext}>{rawMarkdownBody}</Marked>
   </section>
 )
