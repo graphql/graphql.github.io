216f7eca52ea4db03fbee15a8294625d84f4729f
carolstran <hello@workwithcarolyn.com>
2020-12-09 01:59:54 +0100

    Removing unnecessary window checks

--- src/utils/useFAQAccordion.ts
+++ src/utils/useFAQAccordion.ts
@@ -24,18 +24,16 @@ export const useFAQAccordion = () => {
   }
 
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const hash = window.location.hash ? window.location.hash.split("#")[1] : ""
+    const hash = window.location.hash ? window.location.hash.split("#")[1] : ""
 
-      if (hash && buttonCreated) {
-        const anchor = document && document.getElementById(hash)
-        const heading: any = anchor && anchor.parentNode
+    if (hash && buttonCreated) {
+      const anchor = document && document.getElementById(hash)
+      const heading: any = anchor && anchor.parentNode
 
-        if (heading) {
-          heading.parentNode.classList.toggle("open")
-          heading.classList.toggle("open")
-          toggleChildrenClass(heading.parentNode)
-        }
+      if (heading) {
+        heading.parentNode.classList.toggle("open")
+        heading.classList.toggle("open")
+        toggleChildrenClass(heading.parentNode)
       }
     }
   }, [buttonCreated])
@@ -60,14 +58,12 @@ export const useFAQAccordion = () => {
         e.target.localName === "button" ? e.target : e.target.parentNode
 
     
-      if (typeof window !== 'undefined') {
-        window.history.replaceState(
-          {},
-          "",
-          "#" + e.target.getElementsByTagName("a")[0].id
-        )
-        window.history.scrollRestoration = "manual"
-      }
+      window.history.replaceState(
+        {},
+        "",
+        "#" + e.target.getElementsByTagName("a")[0].id
+      )
+      window.history.scrollRestoration = "manual"
 
       if (e.target.localName === "button") {
         e.target.classList.toggle("open")
