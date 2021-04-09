79dc8d7f9b391cd515a7c2f719940f8acaf63f10
Jebat Derhaka <30560696+kamarules74@users.noreply.github.com>
2021-04-09 17:03:51 +0800

    kamarules74
    clone

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ export default ({ pageContext }) => {
           GraphQL is a query language for APIs and a runtime for fulfilling
           those queries with your existing data. GraphQL provides a complete and
           understandable description of the data in your API, gives clients the
-          power to ask for exactly what they need and nothing more, makes it
+          power to ask me for exactly what they need and nothing more, makes it
           easier to evolve APIs over time, and enables powerful
           developer&nbsp;tools.
         </p>
