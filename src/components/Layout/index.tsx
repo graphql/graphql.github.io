787c03589b8f8fbb3dd784049de8e9280a14cc9b
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-03-30 18:34:48 +0200

    boostrap tailwindcss

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,7 +3,7 @@ import Footer from "../Footer"
 import Header from "../Header"
 
 import "../../assets/css/style.less"
-
+import "../../assets/css/global.css"
 interface Props {
   children: React.ReactNode
   className?: string
