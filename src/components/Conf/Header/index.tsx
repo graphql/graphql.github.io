b795e74afea2d59b8510c1a496f1a4731c683f04
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2023-04-03 15:26:33 +0200

    fix header

--- src/components/Conf/Header/index.tsx
+++ src/components/Conf/Header/index.tsx
@@ -13,57 +13,41 @@ const links: LinkItem[] = [
     section: "Register",
     text: "Register",
     href: "/conf/#register",
-    noMobile: true,
   },
-  {
-    section: "Speak",
-    text: "Speak",
-    href: "/conf/speak/",
-  },
+  { section: "Speak", text: "Speak", href: "/conf/speak/" },
   {
     section: "Sponsor",
     text: "Sponsor",
     href: "/conf/sponsor/",
   },
-  {
-    section: "FAQ",
-    text: "FAQ",
-    href: "/conf/faq/",
-  },
+  { section: "FAQ", text: "FAQ", href: "/conf/faq/" },
 ]
 
 const HeaderConf = () => {
   return (
-    <header className="bg-[#0E031C] gap-2 shadow-lg px-5 h-[70px]">
-      <div className="mx-auto flex items-center h-full max-md:justify-between">
-        <a
-          href="/conf/"
-          className="flex order-first max-sm:hidden xl:order-none xl:w-1/5 font-medium items-center xl:items-center xl:justify-center mr-8 xl:mr-0"
-        >
+    <header className="container mx-auto bg-[#0E031C] gap-2 shadow-lg px-5 h-[70px]">
+      <div className="mx-auto flex items-center h-full gap-5">
+        <a href="/conf/" className="shrink-0">
           <img
             src="/img/conf/graphql-conf-logo-simple.svg"
+            className="max-md:w-24"
           />
         </a>
-        <nav className="flex grow items-center justify-between md:justify-normal">
-          {links.map((link, i) => (
-            <a
-              className={`text-sm sm:text-lg text-white font-medium hover:text-white focus:text-white ${
-                link.noMobile ? "max-md:hidden" : ""
-              }`}
-              key={i}
-              href={link.href}
-            >
-              {link.text}
-            </a>
-          ))}
-        </nav>
-        <div>
-          <ButtonConf
-            text="Register Now!"
-            href="https://cvent.me/4zbxz9"
-            target="_blank"
-          />
-        </div>
+        {links.map(link => (
+          <a
+            key={link.href}
+            href={link.href}
+            className="text-sm sm:text-lg text-white font-medium hover:text-white focus:text-white"
+          >
+            {link.text}
+          </a>
+        ))}
+        <ButtonConf
+          text="Register Now!"
+          href="https://cvent.me/4zbxz9"
+          target="_blank"
+          className="ml-auto max-sm:hidden"
+        />
       </div>
     </header>
   )
