9f23412425b40fb3037bbc3580773ae62355b2b4
Lee Byron <lee@leebyron.com>
2023-04-03 09:14:44 +0000

    health faq

--- src/pages/conf/faq.tsx
+++ src/pages/conf/faq.tsx
@@ -76,6 +76,131 @@ const faq: Array<{ id: string; title: string; contents: ReactNode }> = [
     ),
   },
   {
+    id: "health-and-safety",
+    title: "Health & Safety",
+    contents: (
+      <>
+        <em>Updated March 29, 2023</em>
+
+        <p>
+          Our attendees’ health and safety remain our top priority as we
+          continue to monitor COVID-19 and look to local, CDC and WHO guidelines
+          to make the best and most informed decisions around onsite safety and
+          requirements.
+        </p>
+
+        <h3>In-Person Attendance Requirements</h3>
+        <h4>Masks</h4>
+        <p>Masks are recommended, but not required, to be worn at the event.</p>
+
+        <h4>Vaccine Or Negative Covid-19 Test Requirement</h4>
+        <p>
+          At this time, proof of vaccine or a negative COVID test is NOT
+          required to attend the event in person.
+        </p>
+
+        <p>
+          All in-person attendance requirements are subject to change based on
+          local guidelines. Registered attendees will be notified 1 month prior
+          to the event of any updates to our mask mandate and other on-site
+          health & safety requirements.
+        </p>
+
+        <h3>Onsite Health & Safety Measures</h3>
+        <p>Please expect the following measures to be in place at the event.</p>
+        <ul className="list-disc">
+          <li>Event signage indicating required health and safety protocols</li>
+          <li>Onsite cleaning and sanitizing of all touch points</li>
+          <li>Sanitized microphones between each speaker’s use</li>
+          <li>
+            Plentiful sanitation stations onsite and personal hand sanitizer
+            bottles and wipes available for participants
+          </li>
+          <li>
+            Wearable Indicators of Social Distance Comfort Levels — We will
+            provide a wearable indicator of social distance comfort levels for
+            event participants to wear onsite (optional but encouraged).
+            Participants are asked to be respectful of each other’s comfort
+            level on social distance.
+          </li>
+          <li>
+            The Linux Foundation does not allow firearms or other weapons,
+            regardless of whether they are permitted or not, or whether they are
+            concealed or not, to be brought into our events. By registering for
+            the event, you are agreeing that you understand this policy and will
+            not bring a firearm or other weapons into the event.
+          </li>
+        </ul>
+
+        <p>
+          At a minimum, we will be following all mandated venue, municipality,
+          and CDC guidelines. This list, and all protocols, are subject to
+          change should there be a change if local municipality and health
+          agency guidelines indicate or require that they should change. We will
+          do everything we can to provide any updates as early as possible.
+        </p>
+
+        <h3>The United States COVID-Related Entry Requirements</h3>
+        <p>
+          If you are traveling to the United States, review the entry
+          requirements from your country of origin here. With the ever-changing
+          COVID-19 guidelines, we highly suggest visiting{" "}
+          <a
+            href="https://www.cdc.gov/coronavirus/2019-ncov/travelers/international-travel-during-covid19.html?CDC_AA_refVal=https%3A%2F%2Fwww.cdc.gov%2Fcoronavirus%2F2019-ncov%2Ftravelers%2Finternational-travel%2Findex.html"
+            target="_blank"
+          >
+            the CDC website
+          </a>{" "}
+          regularly for the most up-to-date information as your trip nears.
+          Changes and updates are typically marked at the top of the page. Read
+          carefully to ensure you have the most recent information regarding
+          travel rules and regulations based on your country of origin. The
+          requirements may change so continue to check back regularly.
+        </p>
+
+        <h3>What should I do if I am onsite and not feeling well?</h3>
+
+        <p>
+          If you aren’t feeling well, please watch for COVID-19 symptoms.
+          Depending on your symptoms and how unwell you feel, please take an
+          at-home COVID test (we highly recommend traveling with your own tests)
+          or go to the nearest testing facility, urgent care, or hospital.
+        </p>
+
+        <p>
+          If you have chest pain or shortness of breath, we advise you to call
+          911 or go directly to the Emergency Room.
+        </p>
+
+        <h4>Should you end up testing positive for COVID:</h4>
+        <ul className="list-disc">
+          <li>
+            Follow the guidance of the testing facility, urgent care, or
+            hospital that you visited in order to best protect yourself, as well
+            as the community.
+          </li>
+          <li>
+            Please follow the guidance of the testing facility, urgent care, or
+            hospital that you visited in order to best protect yourself, as well
+            as the community. The facility at which you test positive will
+            notify the Department of Health if deemed necessary by local
+            guidelines for contact tracing purposes.
+          </li>
+          <li>You will not be able to return to the event.</li>
+          <li>
+            Email Angela Brown, SVP & GM of Events, at{" "}
+            <a href="mailto:angela@linuxfoundation.org">
+              angela@linuxfoundation.org
+            </a>
+            , to let us know. While we will need to let event participants know
+            that someone onsite tested positive for COVID-19, we will not
+            disclose your name.
+          </li>
+        </ul>
+      </>
+    ),
+  },
+  {
     id: "invoices",
     title: "Invoices & Certificates of Attendance",
     contents: (
@@ -112,6 +237,78 @@ const faq: Array<{ id: string; title: string; contents: ReactNode }> = [
           Please Note: We verify attendance through the registration system, and
           Certificate of Attendance letters are sent out after the event is
           completed.
+        </p>
+      </>
+    ),
+  },
+  {
+    id: "resources",
+    title: "Local Resources",
+    contents: (
+      <>
+        <h3>Taxi</h3>
+        <p>Allied Yellow Cab</p>
+        <p>650-579-7000</p>
+
+        <h3>24-Hour Hospital</h3>
+        <p>
+          <a
+            href="https://www.sutterhealth.org/find-location/facility/mills-peninsula-medical-center"
+            target="_blank"
+          >
+            Mills-Peninsula Medical Center
+          </a>
+        </p>
+        <p>
+          1501 Trousdale Drive
+          <br />
+          Burlingame, CA 94010
+        </p>
+        <p>650-696-5400</p>
+        <p>Dial 911 for Emergencies or Ambulance</p>
+        <p>
+          9-minute drive from venue
+          <br />
+          43-minute walk from venue (2.1 miles)
+        </p>
+
+        <h3>Urgent Care</h3>
+        <p>
+          <a
+            href="https://www.gohealthuc.com/dignity/locations/san-bruno?utm_source=gmb&utm_medium=organic&utm_content=dignity-sanbruno"
+            target="_blank"
+          >
+            Dignity Health – GoHealth Urgent Care
+          </a>
+        </p>
+        <p>
+          1310 El Camino Real Ste I-J
+          <br />
+          San Bruno, CA 94066
+        </p>
+        <p>650-270-2395</p>
+        <p>9-minute drive from venue (5 miles)</p>
+
+        <h3>Pharmacy</h3>
+        <p>
+          <a
+            href="https://www.walgreens.com/topic/promotion/covid-testing.jsp"
+            target="_blank"
+          >
+            Walgreens
+          </a>{" "}
+          (drive-thru COVID-19 testing)
+        </p>
+        <p>
+          45 S. El Camino Real
+          <br />
+          Millbrae, CA 94030
+        </p>
+        <p>800-925-4733</p>
+        <p>
+          6-minute drive from venue
+          <br />
+          39-minute walk from venue (1.9 miles)
         </p>
       </>
     ),
