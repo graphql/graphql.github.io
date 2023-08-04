7e588435720cd97596cbc86026ba4929d547eb20
beerose <alexsandra.sikora@gmail.com>
2023-08-04 13:36:06 +0200

    Add try/catch

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -187,20 +187,24 @@ export const createPages: GatsbyNode["createPages"] = async ({
       })
 
       if (!process.env.GATSBY_CLOUD) {
-        createOpenGraphImage(createPage, {
-          component: path.resolve("./src/templates/EventOgImageTemplate.tsx"),
-          size: {
-            width: 1200,
-            height: 630,
-          },
-          waitCondition: "networkidle0",
-          context: {
-            id: event.id,
-            title: event.name,
-            event,
-            speakers: eventSpeakers,
-          },
-        })
+        try {
+          createOpenGraphImage(createPage, {
+            component: path.resolve("./src/templates/EventOgImageTemplate.tsx"),
+            size: {
+              width: 1200,
+              height: 630,
+            },
+            waitCondition: "networkidle0",
+            context: {
+              id: event.id,
+              title: event.name,
+              event,
+              speakers: eventSpeakers,
+            },
+          })
+        } catch {
+          console.log("Error creating OG image for", event.name)
+        }
       }
     })
 
