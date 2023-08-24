e43dc5a4b7e86f24b7339bcacd0a36ed520a45d7
beerose <alexsandra.sikora@gmail.com>
2023-08-24 11:31:52 +0200

    refactor schedule code a little

--- src/components/Conf/Schedule/ScheduleList.tsx
+++ src/components/Conf/Schedule/ScheduleList.tsx
@@ -1,20 +1,7 @@
 import { format, parseISO, compareAsc } from "date-fns"
-import React, { FC } from "react"
+import React, { FC, useReducer } from "react"
 import { eventsColors } from "../../../utils/eventsColors"
 import { getEventTitle } from "../../../utils/eventTitle"
-
-function groupByKey<T>(arr: T[], getKey: (entity: T) => any) {
-  return Array.from<[string, T[]]>(
-    arr.reduce(
-      (entryMap, e) =>
-        entryMap.set(getKey(e) as keyof T, [
-          ...(entryMap.get(getKey(e) as keyof T) || []),
-          e,
-        ]),
-      new Map()
-    )
-  )
-}
 
 export interface ScheduleSession {
   id: string
@@ -28,9 +15,119 @@ export interface ScheduleSession {
   speakers?: string
 }
 
-type Date = string
-type ConcurrentSessionsGroups = [Date, ScheduleSession[]][]
-type SessionsGroupByDay = [Date, ConcurrentSessionsGroups][]
+export interface ConcurrentSessions {
+  [date: string]: ScheduleSession[]
+}
+
+export interface ScheduleSessionsByDay {
+  [date: string]: ConcurrentSessions
+}
+
+type State = {
+  sessions: ScheduleSessionsByDay | undefined
+}
+type Action =
+  | { type: "set-audience"; tags: string[] }
+  | { type: "set-subtype"; tags: string[] }
+  | { type: "set-eventtype"; tags: string[] }
+  | { type: "reset-filters" }
+const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case "set-audience": {
+      const sessions = Object.entries(state.sessions || {}).reduce(
+        (acc, [date, concurrentSessions]) => {
+          const sessions = Object.entries(concurrentSessions).reduce(
+            (acc, [date, sessions]) => {
+              const filteredSessions = sessions.filter(session =>
+                action.tags.some(tag => session.audience.includes(tag))
+              )
+              return {
+                ...acc,
+                [date]: filteredSessions,
+              }
+            },
+            {} as ConcurrentSessions
+          )
+          return {
+            ...acc,
+            [date]: sessions,
+          }
+        },
+        {} as ScheduleSessionsByDay
+      )
+      return {
+        ...state,
+        sessions,
+      }
+    }
+
+    case "set-subtype": {
+      const sessions = Object.entries(state.sessions || {}).reduce(
+        (acc, [date, concurrentSessions]) => {
+          const sessions = Object.entries(concurrentSessions).reduce(
+            (acc, [date, sessions]) => {
+              const filteredSessions = sessions.filter(session =>
+                action.tags.some(tag => session.event_subtype.includes(tag))
+              )
+              return {
+                ...acc,
+                [date]: filteredSessions,
+              }
+            },
+            {} as ConcurrentSessions
+          )
+          return {
+            ...acc,
+            [date]: sessions,
+          }
+        },
+        {} as ScheduleSessionsByDay
+      )
+      return {
+        ...state,
+        sessions,
+      }
+    }
+
+    case "set-eventtype": {
+      const sessions = Object.entries(state.sessions || {}).reduce(
+        (acc, [date, concurrentSessions]) => {
+          const sessions = Object.entries(concurrentSessions).reduce(
+            (acc, [date, sessions]) => {
+              const filteredSessions = sessions.filter(session =>
+                action.tags.some(tag => session.event_type.includes(tag))
+              )
+              return {
+                ...acc,
+                [date]: filteredSessions,
+              }
+            },
+            {} as ConcurrentSessions
+          )
+          return {
+            ...acc,
+            [date]: sessions,
+          }
+        },
+        {} as ScheduleSessionsByDay
+      )
+      return {
+        ...state,
+        sessions,
+      }
+    }
+
+    case "reset-filters": {
+      return {
+        ...state,
+        sessions: undefined,
+      }
+    }
+
+    default:
+      return state
+  }
+}
 
 interface Props {
   showEventType?: boolean
@@ -42,121 +139,135 @@ const ScheduleList: FC<Props> = ({
   filterSchedule,
   scheduleData,
 }) => {
-  const filteredSortedSchedule = (
-    filterSchedule ? filterSchedule(scheduleData) : scheduleData
-  ).sort((a, b) => compareAsc(new Date(a.event_start), new Date(b.event_start)))
+  const [sessionsState, dispatch] = useReducer<typeof reducer, State>(
+    reducer,
+    {} as State,
+    () => {
+      const data = filterSchedule ? filterSchedule(scheduleData) : scheduleData
 
-  const groupedConcurrentSessions = groupByKey(
-    filteredSortedSchedule,
-    e => e.event_start
+      const filteredSortedSchedule = (data || []).sort((a, b) =>
+        compareAsc(new Date(a.event_start), new Date(b.event_start))
+      )
+
+      const concurrentSessions: ConcurrentSessions = {}
+      filteredSortedSchedule.forEach(session => {
+        if (!concurrentSessions[session.event_start]) {
+          concurrentSessions[session.event_start] = []
+        }
+        concurrentSessions[session.event_start].push(session)
+      })
+
+      const sessionsByDay: ScheduleSessionsByDay = {}
+      Object.entries(concurrentSessions).forEach(([date, sessions]) => {
+        const day = date.split(" ")[0]
+        if (!sessionsByDay[day]) {
+          sessionsByDay[day] = {}
+        }
+        sessionsByDay[day] = {
+          ...sessionsByDay[day],
+          [date]: sessions,
+        }
+      })
+
+      return { sessions: sessionsByDay }
+    }
   )
 
-  const groupedSessionsByDay: SessionsGroupByDay = groupByKey(
-    groupedConcurrentSessions,
-    e => e[0].split(" ")[0]
-  )
-
-  if (groupedSessionsByDay.length === 0) {
+  if (sessionsState.sessions === undefined) {
     return <p>Not available yet. Please check soon.</p>
   }
 
   return (
     <>
-      {groupedSessionsByDay.map(([date, concurrentSessionsGroup]) => (
-        <div key={date} className="text-gray-800 text-sm">
-          <h3 className="mt-10 mb-5">
-            {format(parseISO(date), "EEEE, MMMM d")}
-          </h3>
-          {concurrentSessionsGroup.map(([sharedStartDate, sessions]) => (
-            <div key={`concurrent sessions on ${sharedStartDate}`}>
-              <div className="lg:flex-row flex flex-col mb-4">
-                <div className="relative">
-                  <span className="lg:mr-7 mb-5 whitespace-nowrap text-gray-500 lg:mt-0 mt-3 inline-block lg:w-28 w-20">
-                    {format(parseISO(sharedStartDate), "hh:mmaaaa 'PDT'")}
-                  </span>
-                  <div className="lg:block hidden absolute right-3 top-0 h-full w-0.5 bg-gray-200" />
-                </div>
-                <div className="lg:flex-row flex flex-col gap-5 relative lg:items-start items-end w-full lg:pl-0 pl-[28px]">
-                  <div className="block lg:hidden absolute left-3 top-0 h-full w-0.5 bg-gray-200" />
+      {Object.entries(sessionsState.sessions).map(
+        ([date, concurrentSessionsGroup]) => (
+          <div key={date} className="text-gray-800 text-sm">
+            <h3 className="mt-10 mb-5">
+              {format(parseISO(date), "EEEE, MMMM d")}
+            </h3>
+            {Object.entries(concurrentSessionsGroup).map(
+              ([sessionDate, sessions]) => (
+                <div key={`concurrent sessions on ${sessionDate}`}>
+                  <div className="lg:flex-row flex flex-col mb-4">
+                    <div className="relative">
+                      <span className="lg:mr-7 mb-5 whitespace-nowrap text-gray-500 lg:mt-0 mt-3 inline-block lg:w-28 w-20">
+                        {format(parseISO(sessionDate), "hh:mmaaaa 'PDT'")}
+                      </span>
+                      <div className="lg:block hidden absolute right-3 top-0 h-full w-0.5 bg-gray-200" />
+                    </div>
+                    <div className="lg:flex-row flex flex-col gap-5 relative lg:items-start items-end w-full lg:pl-0 pl-[28px]">
+                      <div className="block lg:hidden absolute left-3 top-0 h-full w-0.5 bg-gray-200" />
 
-                  {sessions.map(session => {
-                    const eventType = session.event_type.endsWith("s")
-                      ? session.event_type.slice(0, -1)
-                      : session.event_type
+                      {sessions.map(session => {
+                        const eventType = session.event_type.endsWith("s")
+                          ? session.event_type.slice(0, -1)
+                          : session.event_type
 
-                    const speakers = session.speakers?.split(",") || []
-                    const eventTitle = getEventTitle(session, speakers)
+                        const speakers = session.speakers?.split(",") || []
+                        const eventTitle = getEventTitle(session, speakers)
 
-                    const [borderColor] = getSessionColor(
-                      session.event_type.toLowerCase()
-                    )
+                        const borderColor = eventsColors[session.event_type]
 
-                    return session.event_type === "Breaks" ? (
-                      <div
-                        key={session.id}
-                        style={{
-                          borderLeft: `10px solid ${borderColor}`,
-                          borderRadius: "5px",
-                          backgroundColor: "white",
-                        }}
-                        className="shadow-[-5px_10px_30px_20px_#d0d3da33] font-normal flex items-center py-2 px-4 rounded-md w-full h-full text-black"
-                      >
-                        {showEventType ? eventType + " / " : ""}
-                        {eventTitle}
-                      </div>
-                    ) : (
-                      <a
-                        id={`session-${session.id}`}
-                        data-tooltip-id="my-tooltip"
-                        href={`/conf/schedule/${session.id}?name=${session.name}`}
-                        key={session.id}
-                        style={{
-                          borderLeft: `10px solid ${borderColor}`,
-                          borderRadius: "5px",
-                          backgroundColor: "white",
-                        }}
-                        className="group no-underline hover:no-underline shadow-[-5px_10px_30px_20px_#d0d3da33] font-normal relative py-2 px-4 rounded-md w-full h-full text-black"
-                      >
-                        <div className="flex flex-col justify-start h-full py-3 gap-y-2">
-                          {borderColor && (
-                            <span
-                              className="group-hover:no-underline flex py-1 px-3 mb-3 self-start justify-center items-center text-white border rounded-3xl"
-                              style={{
-                                backgroundColor: borderColor,
-                              }}
-                            >
-                              {eventType}
-                            </span>
-                          )}
-                          <div className="group-hover:underline flex flex-col justify-between h-full gap-y-2">
+                        return session.event_type === "Breaks" ? (
+                          <div
+                            key={session.id}
+                            style={{
+                              borderLeft: `10px solid ${borderColor}`,
+                              borderRadius: "5px",
+                              backgroundColor: "white",
+                            }}
+                            className="shadow-[-5px_10px_30px_20px_#d0d3da33] font-normal flex items-center py-2 px-4 rounded-md w-full h-full text-black"
+                          >
                             {showEventType ? eventType + " / " : ""}
                             {eventTitle}
-                            {speakers.length > 0 && (
-                              <span className="font-light">
-                                {speakers.join(", ")}
-                              </span>
-                            )}
                           </div>
-                        </div>
-                      </a>
-                    )
-                  })}
+                        ) : (
+                          <a
+                            id={`session-${session.id}`}
+                            data-tooltip-id="my-tooltip"
+                            href={`/conf/schedule/${session.id}?name=${session.name}`}
+                            key={session.id}
+                            style={{
+                              borderLeft: `10px solid ${borderColor}`,
+                              borderRadius: "5px",
+                              backgroundColor: "white",
+                            }}
+                            className="group no-underline hover:no-underline shadow-[-5px_10px_30px_20px_#d0d3da33] font-normal relative py-2 px-4 rounded-md w-full h-full text-black"
+                          >
+                            <div className="flex flex-col justify-start h-full py-3 gap-y-2">
+                              {borderColor && (
+                                <span
+                                  className="group-hover:no-underline flex py-1 px-3 mb-3 self-start justify-center items-center text-white border rounded-3xl"
+                                  style={{
+                                    backgroundColor: borderColor,
+                                  }}
+                                >
+                                  {eventType}
+                                </span>
+                              )}
+                              <div className="group-hover:underline flex flex-col justify-between h-full gap-y-2">
+                                {showEventType ? eventType + " / " : ""}
+                                {eventTitle}
+                                {speakers.length > 0 && (
+                                  <span className="font-light">
+                                    {speakers.join(", ")}
+                                  </span>
+                                )}
+                              </div>
+                            </div>
+                          </a>
+                        )
+                      })}
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      ))}
+              )
+            )}
+          </div>
+        )
+      )}
     </>
   )
 }
 
 export default ScheduleList
-
-function getSessionColor(sessionType: string) {
-  return (
-    eventsColors
-      .find(e => sessionType.includes(e[0].toLowerCase()))
-      ?.slice(1) || []
-  )
-}
--- src/utils/eventsColors.ts
+++ src/utils/eventsColors.ts
@@ -1,8 +1,8 @@
-export const eventsColors = [
-  ["Breaks", "rgb(167, 183, 196)", "rgb(167, 183, 196, 0.1)"],
-  ["Keynote Sessions", "rgb(165, 107, 232)", "rgb(165, 107, 232, 0.1)"],
-  ["Lightning Talks", "rgb(22, 165, 150)", "rgb(22, 165, 150, 0.1)"],
-  ["Session Presentations", "rgb(236, 70, 70)", "rgb(236, 70, 70, 0.1)"],
-  ["Workshops", "rgb(230, 129, 47)", "rgb(230, 129, 47, 0.1)"],
-  ["Unconference", "#A07EFF", "rgb(255, 209, 102, 0.1)"],
-]
+export const eventsColors: Record<string, string> = {
+  Breaks: "#a7b7c4",
+  "Keynote Sessions": "#a56be8",
+  "Lightning Talks": "#16a597",
+  "Session Presentations": "#ec4646",
+  Workshops: "#e6812f",
+  Unconference: "#A07EFF",
+}
