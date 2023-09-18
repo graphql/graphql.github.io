1ae7e72a6ecd4ad518fe69b353b069c15229a633
beerose <alexsandra.sikora@gmail.com>
2023-09-19 00:22:35 +0200

    conf: add day filter

--- src/components/Conf/Schedule/ScheduleList.tsx
+++ src/components/Conf/Schedule/ScheduleList.tsx
@@ -178,94 +178,111 @@ const ScheduleList: FC<Props> = ({
           <h3 className="mb-5">No sessions found</h3>
         </div>
       ) : (
-        Object.entries(sessionsState).map(([date, concurrentSessionsGroup]) => (
-          <div key={date} className="text-gray-800 text-sm">
-            <h3 className="mb-5">{format(parseISO(date), "EEEE, MMMM d")}</h3>
-            {Object.entries(concurrentSessionsGroup).map(
-              ([sessionDate, sessions]) => (
-                <div key={`concurrent sessions on ${sessionDate}`}>
-                  <div className="lg:flex-row flex flex-col mb-4">
-                    <div className="relative">
-                      <span className="lg:mr-7 mb-5 whitespace-nowrap text-gray-500 lg:mt-0 mt-3 inline-block lg:w-28 w-20">
-                        {format(parseISO(sessionDate), "hh:mmaaaa 'PDT'")}
-                      </span>
-                      <div className="lg:block hidden absolute right-3 top-0 h-full w-0.5 bg-gray-200" />
-                    </div>
-                    <div className="lg:flex-row flex flex-col gap-5 relative lg:items-start items-end w-full lg:pl-0 pl-[28px]">
-                      <div className="block lg:hidden absolute left-3 top-0 h-full w-0.5 bg-gray-200" />
+        <>
+          <div className="flex space-x-4">
+            {Object.keys(sessionsState).map((date, index) => (
+              <a
+                href={`#day-${index + 1}`}
+                key={date}
+                className={"text-gray-800 text-xs"}
+              >
+                Day {index + 1}
+              </a>
+            ))}
+          </div>
+          {Object.entries(sessionsState).map(
+            ([date, concurrentSessionsGroup], index) => (
+              <div key={date} className="text-gray-800 text-sm">
+                <h3 className="mb-5" id={`day-${index + 1}`}>
+                  {format(parseISO(date), "EEEE, MMMM d")}
+                </h3>
+                {Object.entries(concurrentSessionsGroup).map(
+                  ([sessionDate, sessions]) => (
+                    <div key={`concurrent sessions on ${sessionDate}`}>
+                      <div className="lg:flex-row flex flex-col mb-4">
+                        <div className="relative">
+                          <span className="lg:mr-7 mb-5 whitespace-nowrap text-gray-500 lg:mt-0 mt-3 inline-block lg:w-28 w-20">
+                            {format(parseISO(sessionDate), "hh:mmaaaa 'PDT'")}
+                          </span>
+                          <div className="lg:block hidden absolute right-3 top-0 h-full w-0.5 bg-gray-200" />
+                        </div>
+                        <div className="lg:flex-row flex flex-col gap-5 relative lg:items-start items-end w-full lg:pl-0 pl-[28px]">
+                          <div className="block lg:hidden absolute left-3 top-0 h-full w-0.5 bg-gray-200" />
 
-                      {sessions.map(session => {
-                        const eventType = session.event_type.endsWith("s")
-                          ? session.event_type.slice(0, -1)
-                          : session.event_type
+                          {sessions.map(session => {
+                            const eventType = session.event_type.endsWith("s")
+                              ? session.event_type.slice(0, -1)
+                              : session.event_type
 
-                        const speakers = session.speakers?.split(",") || []
-                        const eventTitle = getEventTitle(session, speakers)
+                            const speakers = session.speakers?.split(",") || []
+                            const eventTitle = getEventTitle(session, speakers)
 
-                        const borderColor = eventsColors[session.event_type]
+                            const borderColor = eventsColors[session.event_type]
 
-                        return session.event_type === "Breaks" ? (
-                          <div
-                            key={session.id}
-                            style={{
-                              borderLeft: `10px solid ${borderColor}`,
-                              borderRadius: "5px",
-                              backgroundColor: "white",
-                            }}
-                            className="shadow-[-5px_10px_30px_20px_#d0d3da33] font-normal flex items-center py-2 px-4 rounded-md w-full h-full text-black"
-                          >
-                            {showEventType ? eventType + " / " : ""}
-                            {eventTitle}
-                          </div>
-                        ) : (
-                          <a
-                            id={`session-${session.id}`}
-                            data-tooltip-id="my-tooltip"
-                            href={`/conf/schedule/${session.id}?name=${session.name}`}
-                            key={session.id}
-                            style={{
-                              borderLeft: `10px solid ${borderColor}`,
-                              borderRadius: "5px",
-                              backgroundColor: "white",
-                            }}
-                            className="group no-underline hover:no-underline shadow-[-5px_10px_30px_20px_#d0d3da33] font-normal relative py-2 px-4 rounded-md w-full h-full text-black"
-                          >
-                            <div className="flex flex-col justify-start h-full py-3 gap-y-2">
-                              {borderColor && (
-                                <span
-                                  className="group-hover:no-underline flex py-1 px-3 mb-3 self-start justify-center items-center text-white border rounded-3xl"
-                                  style={{
-                                    backgroundColor: borderColor,
-                                  }}
-                                >
-                                  {eventType}
-                                </span>
-                              )}
-                              <div className="group-hover:underline flex flex-col justify-between h-full gap-y-2">
+                            return session.event_type === "Breaks" ? (
+                              <div
+                                key={session.id}
+                                style={{
+                                  borderLeft: `10px solid ${borderColor}`,
+                                  borderRadius: "5px",
+                                  backgroundColor: "white",
+                                }}
+                                className="shadow-[-5px_10px_30px_20px_#d0d3da33] font-normal flex items-center py-2 px-4 rounded-md w-full h-full text-black"
+                              >
                                 {showEventType ? eventType + " / " : ""}
                                 {eventTitle}
-                                <div className="flex flex-col">
-                                  {speakers.length > 0 && (
-                                    <span className="font-light">
-                                      {speakers.join(", ")}
+                              </div>
+                            ) : (
+                              <a
+                                id={`session-${session.id}`}
+                                data-tooltip-id="my-tooltip"
+                                href={`/conf/schedule/${session.id}?name=${session.name}`}
+                                key={session.id}
+                                style={{
+                                  borderLeft: `10px solid ${borderColor}`,
+                                  borderRadius: "5px",
+                                  backgroundColor: "white",
+                                }}
+                                className="group no-underline hover:no-underline shadow-[-5px_10px_30px_20px_#d0d3da33] font-normal relative py-2 px-4 rounded-md w-full h-full text-black"
+                              >
+                                <div className="flex flex-col justify-start h-full py-3 gap-y-2">
+                                  {borderColor && (
+                                    <span
+                                      className="group-hover:no-underline flex py-1 px-3 mb-3 self-start justify-center items-center text-white border rounded-3xl"
+                                      style={{
+                                        backgroundColor: borderColor,
+                                      }}
+                                    >
+                                      {eventType}
                                     </span>
                                   )}
-                                  <span className="font-light">
-                                    Room: {session.venue}
-                                  </span>
+                                  <div className="group-hover:underline flex flex-col justify-between h-full gap-y-2">
+                                    {showEventType ? eventType + " / " : ""}
+                                    {eventTitle}
+                                    <div className="flex flex-col">
+                                      {speakers.length > 0 && (
+                                        <span className="font-light">
+                                          {speakers.join(", ")}
+                                        </span>
+                                      )}
+                                      <span className="font-light">
+                                        Room: {session.venue}
+                                      </span>
+                                    </div>
+                                  </div>
                                 </div>
-                              </div>
-                            </div>
-                          </a>
-                        )
-                      })}
+                              </a>
+                            )
+                          })}
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                </div>
-              )
-            )}
-          </div>
-        ))
+                  )
+                )}
+              </div>
+            )
+          )}
+        </>
       )}
     </>
   )
