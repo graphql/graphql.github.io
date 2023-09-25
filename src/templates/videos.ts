ea5ed51c3a419bce52461f06d9f9084492ab3180
Yassin Eldeeb <yassineldeeb94@gmail.com>
2023-09-25 08:42:20 -0700

    fix: modify the YT mappings generator to fetch all videos through pagination (#1576)

--- src/templates/videos.ts
+++ src/templates/videos.ts
@@ -190,4 +190,388 @@ export const videos = [
     id: "4ao-zjiOGx8",
     title: `The Future of Efficiency Is Here: Add Planning to Your Schema! - Benjie Gillam, Graphile`,
   },
+  {
+    id: "jpy6JEB4LOU",
+    title: `Supercharging GraphQL with Envoy  Unveiling the Efficiency of Declarative GraphQL - Sai Ekbote`,
+  },
+  {
+    id: "sG7D96PdAWg",
+    title: `Launching a Public GraphQL API and Client for Open Source Lightning Web Components - Ben Sklar`,
+  },
+  {
+    id: "m4gkjQjEIBA",
+    title: `The Graph of Everything - Federated Architecture for Any API Service - Uri Goldshtein, The Guild`,
+  },
+  {
+    id: "dwf0mh2b5Rc",
+    title: `Argo  Designing a Compact and Compressible Binary Serialization Format for GraphQL - Mike Solomon`,
+  },
+  {
+    id: "q4Kt0JxrYhw",
+    title: `Mutating Meetup with GraphQL - Annyce Davis, Meetup`,
+  },
+  {
+    id: "k0BiGdpiqWM",
+    title: `GraphQL Fusion  Rethinking Distributed GraphQL - Michael Staib, ChilliCream Inc`,
+  },
+  {
+    id: "XtQiQ0hM574",
+    title: `Can GraphQL be FAIR - Pascal Heus AIData Lead, Postman`,
+  },
+  {
+    id: "8WzgUSXnso0",
+    title: `Semi Concurrent Deduplicated Incremental Delivery - Yaacov Rydzinski`,
+  },
+  {
+    id: "qsUbqJdWgX8",
+    title: `Streamlining Access to NASA Earth Science Data with GraphQL - Ryan Abbott, Element 84`,
+  },
+  {
+    id: "0XiDFUp32gM",
+    title: `Go Beyond the Spec With Custom Directives - Roy Derks, StepZen, an IBM Company`,
+  },
+  {
+    id: "-QTURhHhNB4",
+    title: `GraphQL Over Internet - Denis Badurina, The Guild`,
+  },
+  {
+    id: "VKoFrrHm-hE",
+    title: `Easily Access Blockchain Data with Subgraphs - Kevin Jones, The Graph`,
+  },
+  {
+    id: "Mx1wgZ8G5Vs",
+    title: `GraphQL and CQRS, a Perfect Fit - Gerard Klijs, AxonIQ`,
+  },
+  {
+    id: "hyB2UKsEkqA",
+    title: `GraphQL Security Vulnerabilities in the Wild - Antoine Carossio &amp; Tristan Kalos, Escape`,
+  },
+  {
+    id: "9ZNubCzCwlk",
+    title: `Unlocking Insights: Navigating the World of GraphQL Observability - Eitan Joffe, Inigo`,
+  },
+  {
+    id: "w6SSMwMJ_Xw",
+    title: `8 Years of GraphQL  Unraveling the Trade Offs - Marc Andre Giroux, Netflix`,
+  },
+  {
+    id: "DfAfLqbQLcY",
+    title: `Spoiled for Choice Picking the Right Federation Technique for Your Project -    Bryan Robinson`,
+  },
+  {
+    id: "6A90KYNLAYY",
+    title: `Open Federation - Open Remote Schema Specifications with Other Specs (OpenAPI, etc) - Uri Goldshtein`,
+  },
+  {
+    id: "3zLmU-W2wYA",
+    title: `Welcome Remarks - Lee Byron, GraphQL Foundation`,
+  },
+  {
+    id: "_zNKTWcigyY",
+    title: `The Right Size For GraphQL - Theo Browne, CEO Founder at Ping Labs`,
+  },
+  {
+    id: "KwiaXEEdOWo",
+    title: `Closing Remarks - Lee Byron, GraphQL Foundation`,
+  },
+  {
+    id: "y_2EsTt1-lg",
+    title: `Evolution of Application Networking Patterns with GraphQL - Keith Babo, Solo io`,
+  },
+  { id: "ANg9wvKR7dw", title: `Client Controlled Nullability - 2023-09-15` },
+  {
+    id: "u5iKr-TpK_U",
+    title: `Endre Vegh - GraphQL and caching, when to make it more RESTful`,
+  },
+  { id: "EpC6xmw2a6Y", title: `Dan Starns - GraphQL Observability` },
+  {
+    id: "0wWMXJ4F07A",
+    title: `Tom Harding - GraphQL without Relay is not worth it`,
+  },
+  {
+    id: "IBMf4G7-xE8",
+    title: `Miles Bardon - Improving the GraphQL developer experience on LEGO.com`,
+  },
+  {
+    id: "bmdWEydYas8",
+    title: `Jamie Barton - Unify Data Sources at the Edge with GraphQL`,
+  },
+  {
+    id: "XX05BNMl7lo",
+    title: `Darrell Warde - A journey through the GraphQL validation system`,
+  },
+  {
+    id: "eb4MJh7SHKI",
+    title: `Taz Singh - Guild’s GraphQL Architecture Show &amp; Tell`,
+  },
+  { id: "b72y53Xvjfo", title: `GraphQL Working Group (Primary) - 2023-09-07` },
+  {
+    id: "lnBxsR7bHvQ",
+    title: `Incremental Delivery Working Group - 2023-09-11`,
+  },
+  {
+    id: "2xzIqgu2ba0",
+    title: `Incremental Delivery Working Group - 2023-08-28`,
+  },
+  { id: "9XqTNnSLpgs", title: `Client Controlled Nullability - 2023-08-23` },
+  {
+    id: "OhfgGTtiTf4",
+    title: `Incremental Delivery Working Group - 2023-08-14`,
+  },
+  {
+    id: "AxfbU7VenwI",
+    title: `The Graph of Everything - Federated architecture for any API service - Uri Goldshtein, The Guild`,
+  },
+  {
+    id: "xyGRTWQPuDQ",
+    title: `Scaling with Federation in service to service - Shokhrukh Umriyaev, Senior Engineer at Booking.com`,
+  },
+  {
+    id: "peI3NCsdbB0",
+    title: `GraphQL without Relay is not worth it - Marion Schleifer - Developer Education Lead at Hasura`,
+  },
+  {
+    id: "n9CwYuO4TTs",
+    title: `&quot;GraphQL@Albert Heijn&quot; - Martin van Toorn - [code]capi - Developer at Albert Heijn API platform team`,
+  },
+  {
+    id: "zwzHSvSHOL0",
+    title: `Incremental Delivery Working Group - 2023-08-07`,
+  },
+  { id: "SnEdDStUugA", title: `GraphQL Working Group (Primary) - 2023-08-03` },
+  { id: "AHeuMnBKwNI", title: `Client Controlled Nullability - 2023-07-26` },
+  { id: "bFAyD8AiEcg", title: `Client Controlled Nullability - 2023-07-19` },
+  { id: "eL60cfFw-jo", title: `GraphiQL Working Group - 2023-07-11` },
+  {
+    id: "L-4IXtEwdxM",
+    title: `Incremental Delivery Working Group - 2023-07-10`,
+  },
+  { id: "mHp-hMMSacE", title: `GraphQL Working Group (Primary) - 2023-07-06` },
+  {
+    id: "FoC9S9cathc",
+    title: `Incremental Delivery Working Group - 2023-06-12`,
+  },
+  {
+    id: "utV45PATNX0",
+    title: `Incremental Delivery Working Group - 2023-06-19`,
+  },
+  {
+    id: "I95W0zUCUlk",
+    title: `Incremental Delivery Working Group - 2023-06-26`,
+  },
+  { id: "xSwNZgv6e04", title: `GraphiQL Working Group - 2023-06-13` },
+  {
+    id: "w63vbn5O6nM",
+    title: `Incremental Delivery Working Group - 2023-06-05`,
+  },
+  { id: "_4wwSJmTRhA", title: `GraphQL Working Group (Primary) - 2023-06-01` },
+  {
+    id: "U1iiun6YOmU",
+    title: `Incremental Delivery Working Group - 2023-05-22`,
+  },
+  { id: "VJ5uBPGVDPw", title: `GraphQL Working Group (Primary) - 2023-05-04` },
+  {
+    id: "NHbnz66hsXQ",
+    title: `Incremental Delivery Working Group - 2023-05-08`,
+  },
+  {
+    id: "Bbb2Hwk8cIQ",
+    title: `GraphQL Working Group (Secondary, APAC) - 2023-05-10`,
+  },
+  { id: "TjJJ6SoMXbQ", title: `GraphQL Working Group (Primary) - 2023-04-06` },
+  {
+    id: "5pd2EPePi0A",
+    title: `Incremental Delivery Working Group - 2023-05-01`,
+  },
+  {
+    id: "htqC-oZP53E",
+    title: `Incremental Delivery Working Group - 2023-04-24`,
+  },
+  {
+    id: "v7MCuqHOzN0",
+    title: `Incremental Delivery Working Group - 2023-04-17`,
+  },
+  {
+    id: "vRfkUsPccLc",
+    title: `Incremental Delivery Working Group - 2023-04-03`,
+  },
+  {
+    id: "Dk7zQA0bUVc",
+    title: `GraphQL Working Group (Secondary, EU) - 2023-04-20`,
+  },
+  {
+    id: "h-oVtAS3mRU",
+    title: `Incremental Delivery Working Group - 2023-03-20`,
+  },
+  {
+    id: "yvXjwGbIHJw",
+    title: `Incremental Delivery Working Group - 2023-03-13`,
+  },
+  { id: "HzORPjN-Ojw", title: `GraphQL Working Group (Primary) - 2023-03-02` },
+  {
+    id: "1g_8LzI8Wuk",
+    title: `GraphQL Working Group (Secondary, EU) - 2023-03-16`,
+  },
+  {
+    id: "X-xMmb8dfxE",
+    title: `Incremental Delivery Working Group - 2023-02-27`,
+  },
+  {
+    id: "1xdSMF75o-4",
+    title: `Incremental Delivery Working Group - 2023-02-20`,
+  },
+  { id: "tNvFVhGAbH0", title: `GraphiQL Working Group - 2023-02-14` },
+  {
+    id: "mW2B2qjoaEI",
+    title: `Incremental Delivery Working Group - 2023-02-13`,
+  },
+  {
+    id: "YfXBb22XaO4",
+    title: `GraphQL Working Group (Secondary, APAC) - 2023-02-08`,
+  },
+  { id: "AVb0_6k1gV8", title: `GraphQL Working Group - 2023-02-02` },
+  {
+    id: "4wn5HXLK1zU",
+    title: `GraphQL Working Group (Secondary, EU) - 2023-01-19`,
+  },
+  {
+    id: "ksDDoGUemWM",
+    title: `Incremental Delivery Working Group - 2023-02-06`,
+  },
+  {
+    id: "qtkvaZ5hrTo",
+    title: `Incremental Delivery Working Group - 2023-01-30`,
+  },
+  {
+    id: "DJotY6LQgPE",
+    title: `Incremental Delivery Working Group - 2023-01-16`,
+  },
+  {
+    id: "WFNGwqyrsiw",
+    title: `Incremental Delivery Working Group - 2023-01-23`,
+  },
+  { id: "-GO2S_Gj9G8", title: `GraphQL Working Group - 2023-01-05` },
+  {
+    id: "FcaYzpjJ6eE",
+    title: `GraphQL Working Group (Secondary, APAC) - 2023-01-11`,
+  },
+  { id: "CJDXy5cVcSE", title: `GraphiQL Working Group - 2023-01-10` },
+  { id: "1wsiz2Z0f4w", title: `Incremental Delivery WG - 2022-01-06` },
+  { id: "DyB4jv7v468", title: `GraphQL Working Group - 2022-12-01` },
+  { id: "7w_j0FGcvHY", title: `GraphiQL Working Group - 2022-11-08` },
+  {
+    id: "V2-dh0Mco38",
+    title: `GraphQL Working Group (Secondary, EU) - 2022-11-17`,
+  },
+  {
+    id: "6UFn7M8eRIs",
+    title: `GraphQL Working Group (Secondary, APAC) - 2022-12-07`,
+  },
+  {
+    id: "gvXsnx_9Mv0",
+    title: `GraphQL Working Group (Secondary, APAC) - 2022-11-09`,
+  },
+  {
+    id: "nkPn-F_UBJo",
+    title: `GraphQL Working Group (Secondary, EU) - 2022-12-15`,
+  },
+  { id: "WyGJjcgbM38", title: `GraphiQL Working Group - 2022-12-13` },
+  {
+    id: "SVx4HG2bhII",
+    title: `All for Naught: Client Controlled Nullability - Alex Reilly, Yelp`,
+  },
+  {
+    id: "mzpxqNbsb8w",
+    title: `A Million Messages a Minute - Tanmai Gopal, Hasura`,
+  },
+  {
+    id: "FC4AFSjh_4k",
+    title: `Schema Metadata: A Growing Need - Benjie Gillam, Graphile`,
+  },
+  {
+    id: "EjJ4oDfCpi4",
+    title: `GraphQL Fails Modularity / GraphQL Modularity - Variable... Matt Mahoney, Mofei Zhang, &amp; Dave McCabe`,
+  },
+  {
+    id: "mp7wDoVQU-A",
+    title: `GraphQL Dev Supporting Tools in Meta - Jianfeng Chen, Meta Platform`,
+  },
+  {
+    id: "tNHmvrwJVOc",
+    title: `Welcome &amp; State of GraphQL - Lee Byron, GraphQL Foundation`,
+  },
+  {
+    id: "bNMXoBlh98A",
+    title: `From Reference Implementation to Production - The Parallel Roles... Laurin Quast &amp; Saihajpreet Singh`,
+  },
+  {
+    id: "35kCTfYjoAg",
+    title: `Introducing GraphQL @defer and @stream - Michael Staib, Chillicream &amp; Rob Richard, 1stDibs`,
+  },
+  {
+    id: "PHrAe_pvnT8",
+    title: `The Future of GraphQL IDEs - Thomas Heyenbrock &amp; Tim Suchanek, GraphCDN`,
+  },
+  { id: "JJin9KhFqlk", title: `GraphQL Working Group - 2022-11-03` },
+  {
+    id: "hshIhAGsc2U",
+    title: `GraphQL Working Group (Secondary, EU) - 2022-10-20`,
+  },
+  { id: "LIVhhvLaefg", title: `GraphiQL Working Group - 2022-11-08` },
+  {
+    id: "FJpOiir9DAE",
+    title: `GraphQL Working Group (Secondary, APAC) - 2022-11-09`,
+  },
+  {
+    id: "VvY3l1TleMQ",
+    title: `GraphQL Working Group (Secondary, APAC) - 2022-10-12`,
+  },
+  { id: "SuAEhqWAEco", title: `GraphiQL Working Group - 2022-10-11` },
+  { id: "Ar7tX85NFKE", title: `GraphQL Working Group - 2022-10-06` },
+  { id: "iJWEsVcoNOk", title: `GraphQL.js Working Group - 2022-09-28` },
+  { id: "MEi1u6L__Ck", title: `GraphQL Working Group - 2022-09-01` },
+  { id: "My1dQj1H8Go", title: `Composite Schemas Working Group - 2022-08-17` },
+  { id: "eTlYuy8W_LU", title: `GraphQL Working Group - 2022-08-04` },
+  { id: "1pe6NBhDpNc", title: `GraphiQL Working Group - 2022-07-12` },
+  { id: "oL13y5SyhJ0", title: `GraphQL-over-HTTP Working Group - 2022-07-21` },
+  { id: "dNIT_FTqPt0", title: `Metadata Working Group - 2022-07-28` },
+  { id: "4dTMhDlr1vw", title: `Composite Schemas Working Group - 2022-07-14` },
+  { id: "gVCszBu_aJI", title: `GraphQL Working Group - 2022-07-07` },
+  { id: "kAUZFqN1KDY", title: `GraphQL.js Working Group - 2022-06-29` },
+  { id: "sdF-trSgPPs", title: `GraphQL over HTTP Working Group - 2022-06-27` },
+  { id: "zlsK1oIf5q8", title: `GraphiQL Working Group - 2022-06-14` },
+  { id: "2jgx_vJIY0s", title: `GraphQL Working Group - June 2, 2022` },
+  { id: "G-SMPIKKW2Y", title: `GraphQL.js Working Group - 2022-05-25` },
+  { id: "NcGJ9ol4y9g", title: `GraphiQL Working Group - 2022-05-10` },
+  { id: "6JCbhfWM3Xg", title: `GraphQL.js Working Group - 2022-04-27` },
+  { id: "Dlr65pNpJig", title: `GraphQL Working Group - May 5, 2022` },
+  { id: "9ZPcA1_6D-Y", title: `GraphiQL Working Group - 2022-04-12` },
+  { id: "x2Kv6qrA7Bc", title: `GraphQL Working Group - April 7, 2022` },
+  { id: "GTOYLXsYfHQ", title: `Graphql.js Working Group - 2022-03-30` },
+  { id: "LOf0ATeg_ts", title: `GraphQL js Working Group - 2022-03-08` },
+  { id: "sws7mA2zWS8", title: `GraphQL Working Group - March 3, 2022` },
+  { id: "bMkSaICB4mI", title: `GraphQL Working Group - February 3, 2022` },
+  { id: "IjHBvvlP3qM", title: `GraphQL.js Working Group - 2022-01-26` },
+  { id: "q3maVKYQAlA", title: `GraphQL Working Group - January 6, 2022` },
+  { id: "lTklJxN0wZU", title: `GraphQL Working Group - December 2, 2021` },
+  { id: "J2y64bkTHd8", title: `GraphQL.js Working Group - 2021-11-24` },
+  { id: "hVCzjcP9LAA", title: `GraphQL Working Group - November 4, 2021` },
+  { id: "BBxc_HmB71I", title: `GraphQL.js Working Group - 2021-10-27` },
+  { id: "OQPT2lOpHOE", title: `GraphQL Working Group - October 7, 2021` },
+  { id: "S-VoLuwW1sM", title: `GraphQL.js Working Group - 2021-09-29` },
+  { id: "d6n3NBIWbrs", title: `GraphQL Working Group - September 2, 2021` },
+  { id: "ECe6eMgjoFY", title: `GraphQL.js Working Group - 2021-08-25` },
+  { id: "H2zue-ASKAI", title: `GraphQL.js Working Group - 2021-08-04` },
+  { id: "uIm2ttmGd5E", title: `GraphQL Working Group - July 1, 2021` },
+  { id: "JC2GcmsMy40", title: `GraphQL.js Working Group - 2021-06-30` },
+  { id: "d4HhIo82Whg", title: `GraphQL Working Group - June 3, 2021` },
+  { id: "QjqYWgjxBxY", title: `GraphQL.js Working Group - 2021-05-26` },
+  { id: "KT6B7GhKe6s", title: `GraphQL Working Group - May 13, 2021` },
+  { id: "eok6PeOYls4", title: `GraphQL Working Group - April 1, 2021` },
+  { id: "0iX_R2kIgRc", title: `GraphQL.js Working Group - 2021-03-31` },
+  { id: "R01kuO39Ahg", title: `GraphQL Working Group - March 3, 2021` },
+  { id: "p8EPC7sGBcE", title: `GraphQL Working Group - February 4, 2021` },
+  { id: "pg60TGzxplk", title: `GraphQL.js Working Group - 2021-01-27` },
+  { id: "rLkfz8Snw7M", title: `Input Unions - 2021-01-21` },
+  { id: "pBTKkt3ze_M", title: `GraphQL Working Group - January 7, 2021` },
 ]
