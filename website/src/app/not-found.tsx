616161e522b0b4df92bc660ac1f3e316b9a4a83a
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-03-27 21:15:00 +0100

    add not-found.tsx

---
+++ website/src/app/not-found.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import { usePathname } from "next/navigation"
+
+export default function Page() {
+  const pathname = usePathname()?.replace(/\/$/, "")
+
+  const repo = {
+    origin: "https://github.com",
+    owner: "graphql",
+    name: "graphql.github.io",
+  }
+
+  const title = `Found broken \`${pathname}\` link. Please fix!`
+  const labels = "bug"
+
+  const url = `${repo.origin}/${repo.owner}/${
+    repo.name
+  }/issues/new?title=${encodeURIComponent(title)}&labels=${labels}`
+
+  return (
+    <div className="flex h-screen items-center justify-center flex-col">
+      <h1 className="text-white text-4xl">404: Page Not Found</h1>
+      <a
+        href={url}
+        target="_blank"
+        rel="noreferrer"
+        className="mt-6 text-primary underline decoration-from-font [text-underline-position:from-font]"
+      >
+        Submit an issue about broken link →
+      </a>
+    </div>
+  )
+}
