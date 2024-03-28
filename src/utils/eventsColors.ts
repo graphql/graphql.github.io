874f8b4a0fc1c54edc833698ecb18db2dd66714e
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-03-28 13:09:24 +0100

    remove old `source` files (#1635)

--- src/utils/eventsColors.ts
+++
@@ -1,8 +0,0 @@
-export const eventsColors: Record<string, string> = {
-  Breaks: "#a7b7c4",
-  "Keynote Sessions": "#a56be8",
-  "Lightning Talks": "#16a597",
-  "Session Presentations": "#ec4646",
-  Workshops: "#e6812f",
-  Unconference: "#A07EFF",
-}
