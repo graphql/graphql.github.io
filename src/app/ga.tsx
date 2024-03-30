6c6afe34425b4377707e231c836ca6bbc19cc865
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-03-30 19:36:39 +0100

    add for app router as well

---
+++ src/app/ga.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import Script from "next/script"
+
+export default function GoogleAnalytics() {
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+              page_path: location.pathname,
+            });
+          `,
+        }}
+      />
+    </>
+  )
+}
