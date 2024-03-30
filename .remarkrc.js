b3ea62e0c4588bf0441e4585b8f3fea924cc3da8
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-03-30 15:11:00 +0100

    more

--- .remarkrc.js
+++ .remarkrc.js
@@ -1,6 +1,6 @@
 export default {
   plugins: [
     "frontmatter", // Required to parse frontmatter for linting
-    "remark-lint-first-heading-level"
+    "remark-lint-first-heading-level",
   ],
 }
