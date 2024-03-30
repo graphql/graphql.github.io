89e8ae26be686c3cfa76dbd3b21766def1e62468
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-03-30 19:13:39 +0100

    move spec to community

--- src/pages/community/_meta.tsx
+++ src/pages/community/_meta.tsx
@@ -8,4 +8,9 @@ export default {
   events: "",
   contribute: "Contribute to GraphQL",
   foundation: "Foundation",
+  spec: {
+    title: <span className="after:content-['_↗'] after:font-sans">Spec</span>,
+    href: "https://spec.graphql.org",
+    newWindow: true,
+  },
 }
