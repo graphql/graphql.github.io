3af22800423d0a5d705a2f791ff9c261bac8fb2f
YassinEldeeb <yassineldeeb94@gmail.com>
2024-08-15 12:50:31 +0100

    fix

--- src/app/conf/2024/schedule/[id]/page.tsx
+++ src/app/conf/2024/schedule/[id]/page.tsx
@@ -119,10 +119,10 @@ export default function SessionPage({ params }: SessionProps) {
                     - {format(parseISO(event.event_end), "hh:mmaaaa 'PDT'")}
                   </span>
                 </div>
-                <div className="flex lg:flex-row flex-col sm:gap-5">
+                <div className="flex flex-wrap lg:flex-row flex-col gap-5 mt-8">
                   {event.speakers!.map(speaker => (
                     <div
-                      className="flex items-center gap-3"
+                      className={`flex items-center gap-3 w-full ${event?.speakers?.length || 0 > 1 ? "max-w-[320px]": ""}`}
                       key={speaker.username}
                     >
                       <Avatar
