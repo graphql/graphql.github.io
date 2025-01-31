d1fc0ddc88c8bb7cf230bfd8cee647446e221bec
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2025-01-31 14:41:09 +0000

    open external links in new tab

--- src/app/conf/2025/resources/client-mdx.tsx
+++ src/app/conf/2025/resources/client-mdx.tsx
@@ -1,3 +1,21 @@
 "use client"
 
-export { default } from "./resources.mdx"
+import Resources from "./resources.mdx"
+
+export default function ResourcesPage() {
+  return (
+    <Resources
+      components={{
+        // @ts-expect-error - not sure how to type this
+        a: props => {
+          const isExternal = props.href.startsWith("http")
+          if (isExternal) {
+            return <a {...props} target="_blank" rel="noopener noreferrer" />
+          }
+
+          return <a {...props} />
+        },
+      }}
+    />
+  )
+}
