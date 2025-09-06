88d0598b99776f847ec3f9218c22405bd5f8cf8b
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-09-06 22:50:23 +0200

    Make mdx link underline more visible

--- src/_design-system/mdx-components/mdx-link.tsx
+++ src/_design-system/mdx-components/mdx-link.tsx
@@ -12,7 +12,7 @@ export const MdxLink = forwardRef<
       {...props}
       ref={ref}
       className={clsx(
-        "typography-link text-neu-900 decoration-from-font underline-offset-2",
+        "typography-link text-neu-900 underline-offset-2",
         props.className,
       )}
       href={props.href || ""}
