40e63a000acde0ff4e092d9741467fad4ccd838b
Jonathan Brennan <jonathanawesome@users.noreply.github.com>
2025-11-06 19:13:52 -0600

    fix fetchpriority warning

--- src/app/(conf)/conf/2025/components/testimonials/index.tsx
+++ src/app/(conf)/conf/2025/components/testimonials/index.tsx
@@ -115,8 +115,7 @@ export function TestimonialAuthor({
           width={128}
           height={128}
           className="size-16 saturate-[.1] dark:opacity-90 xl:size-32"
-          // @ts-expect-error React doesn't know it exists yet, but @types/react do
-          fetchpriority="low"
+          fetchPriority="low"
           loading="lazy"
         />
         <div className="absolute inset-0 z-[1] bg-pri-darker opacity-80 mix-blend-plus-lighter dark:mix-blend-color" />
