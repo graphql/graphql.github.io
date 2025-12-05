9d2d29bd9699eff323324cde567f1c8b87ed598c
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-12-05 21:53:38 +0100

    Make LearnHeroStripes configurable

--- src/components/learn-aggregator/learn-hero-stripes.tsx
+++ src/components/learn-aggregator/learn-hero-stripes.tsx
@@ -1,13 +1,24 @@
+import { clsx } from "clsx"
 import { StripesDecoration } from "@/app/conf/_design-system/stripes-decoration"
 
 import blurBean from "./learn-blur-bean.webp"
 
-export function LearnHeroStripes() {
+export function LearnHeroStripes({
+  className,
+  style,
+  ...rest
+}: {
+  className?: string
+  style?: React.CSSProperties
+}) {
   return (
     <div
       role="presentation"
       // eslint-disable-next-line tailwindcss/no-contradicting-classname
-      className="pointer-events-none absolute inset-0 bg-neu-50 [--end-1:#FFF] [--end-2:rgb(255_204_239/.2)] [--start-1:#FFEAF8] [--start-2:hsl(var(--color-sec-lighter))] dark:[--end-1:hsl(75,15%,5%)] dark:[--end-2:hsl(319,100%,10%)] dark:[--start-1:hsl(319,100%,14%)] dark:[--start-2:hsl(319,100%,30%)] sm:h-[360px] lg:h-[calc(100%-163px)]"
+      className={clsx(
+        "pointer-events-none absolute inset-0 bg-neu-50 [--end-1:#FFF] [--end-2:rgb(255_204_239/.2)] [--start-1:#FFEAF8] [--start-2:hsl(var(--color-sec-lighter))] dark:[--end-1:hsl(75,15%,5%)] dark:[--end-2:hsl(319,100%,10%)] dark:[--start-1:hsl(319,100%,14%)] dark:[--start-2:hsl(319,100%,30%)] sm:h-[360px] lg:h-[calc(100%-163px)]",
+        className,
+      )}
       style={{
         maskImage: `url(${blurBean.src})`,
         WebkitMaskImage: `url(${blurBean.src})`,
@@ -17,7 +28,9 @@ export function LearnHeroStripes() {
         WebkitMaskPosition: "50% 60%",
         maskRepeat: "no-repeat",
         WebkitMaskRepeat: "no-repeat",
+        ...style,
       }}
+      {...rest}
     >
       <StripesDecoration
         evenClassName="bg-[linear-gradient(180deg,var(--start-1)_-80%,var(--end-1)_102%)]"
