a7465ab945ce6b279fda1b1007a00808d0cd2d2c
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-12-05 21:19:14 +0100

    Add ResourceMetadata schema

--- src/resources/types.ts
+++ src/resources/types.ts
@@ -1,3 +1,5 @@
+import { type } from "arktype"
+
 export const topics = [
   "frontend",
   "backend",
@@ -15,3 +17,14 @@ export const kinds = ["video", "blog", "tools-and-libraries", "guide"] as const
 export type Kind = (typeof kinds)[number]
 
 export type ResourceTag = Topic | Kind
+
+export const ResourceMetadata = type({
+  title: "string>0",
+  url: "string.url",
+  "kind?": type.enumerated(kinds),
+  "topics?": type.enumerated(topics).array(),
+  "description?": "string>0",
+  tags: type.enumerated([...topics, ...kinds]).array(),
+})
+
+export type ResourceMetadata = typeof ResourceMetadata.inferOut
