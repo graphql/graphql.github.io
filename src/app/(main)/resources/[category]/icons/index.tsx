88ac46e52cb9847d32230e2623648200805f0030
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-12-12 18:44:56 +0100

    Make a spritesheet

--- src/app/(main)/resources/[category]/icons/index.tsx
+++ src/app/(main)/resources/[category]/icons/index.tsx
@@ -1,41 +1,35 @@
-import type { ComponentType, SVGProps } from "react"
+import type { SVGProps } from "react"
 
-import Ballerina from "./ballerina.svg?svgr"
-import CNet from "./c-net.svg?svgr"
-import Clojure from "./clojure.svg?svgr"
-import Elixir from "./elixir.svg?svgr"
-import Elm from "./elm.svg?svgr"
-import Flutter from "./flutter.svg?svgr"
-import GoIcon from "./go.svg?svgr"
-import Haskell from "./haskell.svg?svgr"
-import JavaKotlinAndroid from "./java-kotlin-android.svg?svgr"
-import Javascript from "./javascript.svg?svgr"
-import Julia from "./julia.svg?svgr"
-import LanguageSupport from "./language-support.svg?svgr"
-import Php from "./php.svg?svgr"
-import Python from "./python.svg?svgr"
-import Ruby from "./ruby.svg?svgr"
-import Rust from "./rust.svg?svgr"
-import Scala from "./scala.svg?svgr"
-import SwiftObjectiveCIos from "./swift-objective-c-ios.svg?svgr"
+const sprite = new URL("./sprite.svg", import.meta.url).href
 
-export const icons = {
-  ballerina: Ballerina,
-  "c-net": CNet,
-  clojure: Clojure,
-  elixir: Elixir,
-  elm: Elm,
-  flutter: Flutter,
-  go: GoIcon,
-  haskell: Haskell,
-  "java-kotlin-android": JavaKotlinAndroid,
-  javascript: Javascript,
-  julia: Julia,
-  "language-support": LanguageSupport,
-  php: Php,
-  python: Python,
-  ruby: Ruby,
-  rust: Rust,
-  scala: Scala,
-  "swift-objective-c-ios": SwiftObjectiveCIos,
-} satisfies Record<string, ComponentType<SVGProps<SVGElement>>>
+export type IconName =
+  | "ballerina"
+  | "c-net"
+  | "clojure"
+  | "elixir"
+  | "elm"
+  | "flutter"
+  | "go"
+  | "haskell"
+  | "java"
+  | "javascript"
+  | "julia"
+  | "multiplatform"
+  | "php"
+  | "python"
+  | "ruby"
+  | "rust"
+  | "scala"
+  | "swift"
+
+interface IconProps extends SVGProps<SVGSVGElement> {
+  icon: IconName
+}
+
+export function Icon({ icon, ...props }: IconProps) {
+  return (
+    <svg {...props}>
+      <use href={`${sprite}#${icon}`} />
+    </svg>
+  )
+}
