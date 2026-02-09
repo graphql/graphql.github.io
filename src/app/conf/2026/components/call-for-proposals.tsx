cee7fbb1a22c51127e382f6b967072c012b90ba0
Jory Burson <jory@thestoryofjory.com>
2026-02-09 12:49:03 -0500

    fixup
    Feb 11 is Wednesday not Monday

--- src/app/conf/2026/components/call-for-proposals.tsx
+++ src/app/conf/2026/components/call-for-proposals.tsx
@@ -13,7 +13,7 @@ function DatesTab() {
       <DefinitionListItem term="CFP Opens" definition="Thursday, 16 January" />
       <DefinitionListItem
         term="CFP Close"
-        definition="Monday, February 11, 11:59 PM PST (UTC-8)"
+        definition="Wednesday, February 11, 11:59 PM PST (UTC-8)"
       />
       <DefinitionListItem
         term="CFP Notifications"
